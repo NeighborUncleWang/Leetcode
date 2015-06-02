96521b1651b7c8fa3bfac56be206cf4cfd0b2f3e
NeighborUncleWang <lixianshu1992@gmail.com>
2015-06-02 00:42:22 -0500

    indention the code for 27_RemoveElement.cpp

--- Cpp/27_RemoveElement.cpp
+++ Cpp/27_RemoveElement.cpp
@@ -1,13 +1,13 @@
-class Solution {
-	public:
+class Solution {	
+public:
     int removeElement(vector<int>& nums, int val) {
-	        for (auto it = nums.begin(); it < nums.end(); ++it) {
-			            if (*it == val) {
-						                nums.erase(it);
-						                --it;
-						            }
-			        }
-	        return nums.size();
-	    }
+		for (auto it = nums.begin(); it < nums.end(); ++it) {
+			if (*it == val) {
+				nums.erase(it);	               
+				--it;
+			}
+		}
+		return nums.size();
+	}
 };
 
