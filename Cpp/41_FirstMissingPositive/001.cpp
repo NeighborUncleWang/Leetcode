cb71821efbe224c74274971f20a10f4be06e1b0c
NeighborUncleWang <lixianshu1992@gmail.com>
2015-06-03 00:35:36 -0500

    delete redundent code in firstMissingPositive

--- Cpp/41_FirstMissingPositive/001.cpp
+++ Cpp/41_FirstMissingPositive/001.cpp
@@ -5,11 +5,7 @@ public:
         for (int i = 0; i < n; ++i) {
             while (nums[i] > 0 && nums[i] <= n
                    && nums[i] != nums[nums[i] - 1]) {
-                if (nums[nums[i] - 1] == nums[i]) {
-                    break;
-                }//I add this code block in case nums[1] = 4 and nums[3] = 4
-                //to prevent infinite loop
-                swap(nums[i], nums[nums[i] - 1]);
+				swap(nums[i], nums[nums[i] - 1]);
             }
         }
         for (int i = 0; i < n; ++i) {
