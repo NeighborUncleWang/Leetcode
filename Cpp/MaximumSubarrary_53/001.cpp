a134d006dde191ddd469cfc98e6a52b8891246e8
NeighborUncleWang <lixianshu1992@gmail.com>
2015-06-12 18:08:12 -0500

    .

---
+++ Cpp/MaximumSubarrary_53/001.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int local = nums[0], global = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            local = max(nums[i], local + nums[i]);
+            global = max(global, local);
+        }
+        return global;
+    }
+};
