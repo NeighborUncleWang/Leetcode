6bc98c08cd9df9b908d865039441fb33227d75d6
NeighborUncleWang <lixianshu1992@gmail.com>
2015-06-17 16:17:40 -0500

    solution to 119

---
+++ Cpp/119_Pascal'sTriangleII/001.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> result(rowIndex + 1, 0);
+        for (int i = 0; i <= rowIndex; ++i) {
+            for (int j = i; j >= 0; --j) {
+                if (j == 0) {
+                    result[j] = 1;
+                } else {
+                    result[j] = result[j] + result[j - 1];
+                }
+            }
+        }
+        return result;
+    }
+};
---
+++ Cpp/119_Pascal'sTriangleII/readme.txt
@@ -0,0 +1,3 @@
+Time complexity: O(n2)
+Space complexity: O(n)
+Use only one vector to store the result. Remember to calculate the element in the vector from back to front so that the previous value will not be eliminated when you need to use it. 001.cpp is just 2 lines shorter than 002.cpp
