bba86208597216e43b9ef09d7b05376cb72dfc50
NeighborUncleWang <lixianshu1992@gmail.com>
2015-06-20 18:41:10 -0500

    .

--- Cpp/045_JumpGameII/001.cpp
+++ Cpp/045_JumpGameII/001.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> result(n, INT_MAX);
-        result[0] = 0;
         int maxLength = 0, step = 0, lastReach = 0;
         for (int i = 0; i < n; ++i) {
             if (i > lastReach) {
