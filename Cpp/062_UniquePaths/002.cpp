ccaa9daf04580a07824ad0f2701f0f399f68a0d9
NeighborUncleWang <lixianshu1992@gmail.com>
2015-06-21 21:42:26 -0500

    .

--- Cpp/062_UniquePaths/002.cpp
+++ Cpp/062_UniquePaths/002.cpp
@@ -4,10 +4,8 @@ public:
         vector<int> result(n, 0);
         result[0] = 1;
         for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (0 != j) {
-                    result[j] += result[j - 1];
-                }
+            for (int j = 1; j < n; ++j) {
+                result[j] += result[j - 1];
             }
         }
         return result[n - 1];
