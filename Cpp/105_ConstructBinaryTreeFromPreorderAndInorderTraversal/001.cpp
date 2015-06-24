651a923f3a1b62e0282263ce03fead5407188e66
NeighborUncleWang <lixianshu1992@gmail.com>
2015-06-23 22:46:45 -0500

    solution to Q105

---
+++ Cpp/105_ConstructBinaryTreeFromPreorderAndInorderTraversal/001.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n = preorder.size();
+        unordered_map<int, int> hashMap;
+        for (int i = 0; i < n; ++i) {
+            hashMap[inorder[i]] = i;
+        }
+        return buildTreeHelper(preorder, 0, n - 1, inorder, 0, n - 1, hashMap);
+    }
+    TreeNode* buildTreeHelper(const vector<int>& preorder, int preLeft, int preRight,
+    const vector<int>& inorder, int inLeft, int inRight, const unordered_map<int, int>& hashMap) {
+        if (preLeft > preRight) {
+            return NULL;
+        }
+        TreeNode* root = new TreeNode(preorder[preLeft]);
+        auto it = hashMap.find(preorder[preLeft]);
+        int index = it->second;
+        root->left = buildTreeHelper(preorder, preLeft + 1, preLeft + index - inLeft,
+        inorder, inLeft, index - 1, hashMap);
+        root->right = buildTreeHelper(preorder, preLeft + index - inLeft + 1, preRight,
+        inorder, index + 1, inRight, hashMap);
+        return root;
+    }
+};
---
+++ Cpp/105_ConstructBinaryTreeFromPreorderAndInorderTraversal/readme.txt
@@ -0,0 +1,3 @@
+001.cpp:
+Time complexity: O(n)
+Space complexity: O(n) (The hash map needs O(n) memory, the recursive stack use O(n) memory, if you use recursive to solve this problem, I think there is no way to do with O(1) memory)
