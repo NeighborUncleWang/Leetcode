c84c9c3bf5baf540dd05bb735673b03adf910afc
NeighborUncleWang <lixianshu1992@gmail.com>
2015-06-24 02:01:27 -0500

    solution to Q106

---
+++ Cpp/106_ConstructBinaryTreeFromInorderAndPostorderTraversal/001.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        int n = inorder.size();
+        unordered_map<int, int> hashMap;
+        for (int i = 0; i < n; ++i) {
+            hashMap[inorder[i]] = i;
+        }
+        return buildTreeHelper(inorder, 0, n - 1, postorder, 0, n - 1, hashMap);
+    }
+    TreeNode* buildTreeHelper(const vector<int>& inorder, int inLeft, int inRight,
+    const vector<int>& postorder, int postLeft, int postRight, unordered_map<int, int>& hashMap) {
+        if (postLeft > postRight) {
+            return NULL;
+        }
+        TreeNode* root = new TreeNode(postorder[postRight]);
+        auto it = hashMap.find(postorder[postRight]);
+        int index = it->second;
+        root->left = buildTreeHelper(inorder, inLeft, index - 1, postorder, postLeft, postLeft + index - inLeft - 1, hashMap);
+        root->right = buildTreeHelper(inorder, index + 1, inRight, postorder, postLeft + index - inLeft, postRight - 1, hashMap);
+        return root;
+    }
+};
---
+++ Cpp/106_ConstructBinaryTreeFromInorderAndPostorderTraversal/readme.txt
@@ -0,0 +1,3 @@
+001.cpp
+Time complexity: O(n)
+Space complexity: O(n)(both hash map and recursive stack need O(n) memory)
