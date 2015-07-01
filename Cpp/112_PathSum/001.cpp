a7bdb2475dc1f58ef4a000e9dddc714b7253fff9
NeighborUncleWang <lixianshu1992@gmail.com>
2015-07-01 15:11:08 -0500

    solution to Q122

---
+++ Cpp/112_PathSum/001.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if (root == nullptr) {
+            return false;
+        } else if (root->right == nullptr && root->left == nullptr
+        && sum == root->val) {
+            return true;
+        } else {
+            return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
+        }
+    }
+};
