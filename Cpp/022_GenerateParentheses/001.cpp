cc9b73dc7e8a8d3a9292d8ca188d015a6c550d2f
NeighborUncleWang <lixianshu1992@gmail.com>
2015-07-08 20:38:51 -0500

    solution to Q022

---
+++ Cpp/022_GenerateParentheses/001.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        if (n == 0) {
+            return result;
+        }
+        string sequence;
+        helper(sequence, result, 0, 0, 0, n);
+        return result;
+    }
+private:
+    void helper(string& sequence, vector<string>& result, int leftNumber, int rightNumber, int deep, int n) {
+        if (deep == 2 * n) {//actually we can test(rightNum == n && leftNum == n) here
+            result.push_back(sequence);
+            return;
+        }
+        if (leftNumber < n) {//actually the if (leftNumber < n) and if (rightNumber < leftNumber) can swap their order(first test rightNumber < leftNumber)
+            sequence.push_back('(');
+            helper(sequence, result, leftNumber + 1, rightNumber, deep + 1, n);
+            sequence.pop_back();
+        }
+        if (rightNumber < leftNumber) {
+            sequence.push_back(')');
+            helper(sequence, result, leftNumber, rightNumber + 1, deep + 1, n);
+            sequence.pop_back();
+        }
+    }
+};
