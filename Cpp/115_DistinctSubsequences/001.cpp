98cda00822ca85010d51dcdd873e76d114ec97cc
NeighborUncleWang <lixianshu1992@gmail.com>
2015-07-21 14:39:06 -0500

    solution to Q115

---
+++ Cpp/115_DistinctSubsequences/001.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+        vector<int> result(n + 1, 0);
+        result[0] = 1;
+        for (int i = 1; i < m + 1; ++i) {
+            for (int j = n; j >= 1; --j) {
+                //记得在?:两边加（），否则会变成？result[j - 1] : 0 + result[j]
+                //或者写成result[j] += s[i - 1] == t[j - 1] ? result[j - 1] : 0;
+                result[j] = (s[i - 1] == t[j - 1] ? result[j - 1] : 0) + result[j];
+            }
+        }
+        return result.back();
+    }
+};
---
+++ Cpp/115_DistinctSubsequences/readme.txt
@@ -0,0 +1,3 @@
+001.cpp:
+Time complexity: O(m * n)
+Space complexity: O(n)(这里没办法变成O(min(m, n)), 否则行列顺序要换)
