8fa5376f7c93743dc5d5872ba04c3c8c0897f57c
NeighborUncleWang <lixianshu1992@gmail.com>
2015-07-22 00:48:53 -0500

    solution to Q139

---
+++ Cpp/139_WordBreak/001.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool wordBreak(string s, unordered_set<string>& wordDict) {
+        int n = s.size();
+        vector<bool> result(n + 1, false);
+        result[0] = true;
+        for (int i = 1; i < n + 1; ++i) {
+            for (int j = 0; j < i + 1; ++j) {
+                result[i] = result[i] || result[j] && wordDict.find(s.substr(j, i - j)) != wordDict.end();
+            }
+        }
+        return result[n];
+    }
+};
---
+++ Cpp/139_WordBreak/readme.txt
@@ -0,0 +1,4 @@
+001.cpp:
+Time complexity: O(n^2)
+Space complexity: O(n)
+http://blog.csdn.net/linhuanmars/article/details/22358863
