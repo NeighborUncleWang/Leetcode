f3b21e4c3e9c656d278b26163356a97d0160aac4
NeighborUncleWang <lixianshu1992@gmail.com>
2015-08-12 19:13:23 -0500

    solution to Q198

--- Cpp/198_HouseRobber/001.cpp
+++ Cpp/198_HouseRobber/001.cpp
@@ -4,14 +4,14 @@ public:
         if (nums.size() < 2) {
             return nums.size() == 0 ? 0 : nums[0];
         }
-        vector<int> dp(nums.size(), 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        int fn_2 = nums[0];
+        int fn_1 = max(nums[0], nums[1]);
+        int result = fn_1;
         for (int i = 2; i < nums.size(); ++i) {
-            //dp[i - 2] + nums[i] means rob ith house
-            //dp[i - 1] means don't rob ith house
-            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+            result = max(fn_2 + nums[i], fn_1);
+            fn_2 = fn_1;
+            fn_1 = result;
         }
-        return dp.back();
+        return result;
     }
 };
