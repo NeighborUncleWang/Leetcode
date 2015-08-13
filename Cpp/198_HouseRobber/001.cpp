6c18251147ba687e850c8e282b59b678e3f9c87f
NeighborUncleWang <lixianshu1992@gmail.com>
2015-08-12 19:23:56 -0500

    solution to Q198

--- Cpp/198_HouseRobber/001.cpp
+++ Cpp/198_HouseRobber/001.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if (nums.size() < 2) {
-            return nums.size() == 0 ? 0 : nums[0];
-        }
-        int fn_2 = nums[0];
-        int fn_1 = max(nums[0], nums[1]);
+        int fn_2 = 0;
+        int fn_1 = 0;
         int result = fn_1;
-        for (int i = 2; i < nums.size(); ++i) {
+        for (int i = 0; i < nums.size(); ++i) {
             result = max(fn_2 + nums[i], fn_1);
             fn_2 = fn_1;
             fn_1 = result;
