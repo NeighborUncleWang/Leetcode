ef1fbe60cca06a261f4b2884e8c4970b2c930e57
NeighborUncleWang <lixianshu1992@gmail.com>
2015-08-17 12:11:17 -0500

    solution to Q257

---
+++ Cpp/257_BinaryTreePaths/001.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> result;
+        string path;
+        helper(root, path, result);
+        return result;
+    }
+private:
+    void helper(TreeNode* node, string path, vector<string>& result) {
+        if (node == nullptr) {
+            return;
+        } else if (node->left == nullptr && node->right == nullptr) {
+            path += to_string(node->val);
+            result.push_back(path);
+            return;
+        }
+        path += to_string(node->val) + "->";
+        helper(node->left, path, result);
+        helper(node->right, path, result);
+    }
+};
---
+++ Cpp/257_BinaryTreePaths/readme.txt
@@ -0,0 +1,3 @@
+001.cpp:
+Time complexity: O(2^h)
+Space complexity: O(2^h)
