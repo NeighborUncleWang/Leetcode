cd4d2d03882a0d48c6863fa4d470d90b9cb82774
Xianshu Li <lixianshu1992@gmail.com>
2015-09-27 16:11:53 -0500

    change the mapping function to be a hash table

--- Cpp/013_RomanToInteger/001.cpp
+++ Cpp/013_RomanToInteger/001.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
     int romanToInt(string s) {
+        unordered_map<char, int> dictionary{
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+            {'C', 100}, {'D', 500}, {'M', 1000}
+        };
         int result = 0;
         for (int i = 0; i < s.size(); ++i) {
-            if (i > 0 && romanCharTable(s[i]) > romanCharTable(s[i - 1])) {
-                result = result - 2 * romanCharTable(s[i - 1]);
+            if (i < s.size() - 1 && dictionary[s[i]] < dictionary[s[i + 1]]) {
+                result -= 2 * dictionary[s[i]];
             }
-            result += romanCharTable(s[i]);
+            result += dictionary[s[i]];
         }
         return result;
-    }
-private:
-    inline int romanCharTable(char s) {
-        switch (s) {
-            case 'I':
-                return 1;
-            case 'V':
-                return 5;
-            case 'X':
-                return 10;
-            case 'L':
-                return 50;
-            case 'C':
-                return 100;
-            case 'D':
-                return 500;
-            case 'M':
-                return 1000;
-        }
     }
 };
