e3898c84a2830527ee2caca4a2e17e048e30c79c
Xianshu Li <lixianshu1992@gmail.com>
2015-12-04 15:14:15 -0600

    add 001.cpp

---
+++ Cpp/251_Flatten2DVector/001.cpp
@@ -0,0 +1,35 @@
+class Vector2D {
+public:
+    Vector2D(vector<vector<int>>& vec2d) {
+        xPosition = 0;
+        yPosition = 0;
+        matrix = vec2d;
+        validateIndices();
+    }
+    void validateIndices(void) {
+        while (xPosition < matrix.size() && yPosition >= matrix[xPosition].size()) {
+            ++xPosition;
+            yPosition = 0;
+        }
+    }
+    int next() {
+        int temp = matrix[xPosition][yPosition];
+        ++yPosition;
+        validateIndices();
+        return temp;
+    }
+
+    bool hasNext() {
+        return xPosition < matrix.size();
+    }
+private:
+    int xPosition;
+    int yPosition;
+    vector<vector<int>> matrix;
+};
+
+/**
+ * Your Vector2D object will be instantiated and called as such:
+ * Vector2D i(vec2d);
+ * while (i.hasNext()) cout << i.next();
+ */
---
+++ Cpp/251_Flatten2DVector/readme.txt
@@ -0,0 +1,4 @@
+001.cpp:
+Time complexity: O(1)
+Space complexity: O(1)
+https://leetcode.com/discuss/71821/c-easy-to-understand
