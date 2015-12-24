df08a3f5ddfda70a2b88f226ad594393c018b678
Xianshu Li <lixianshu1992@gmail.com>
2015-12-23 22:20:05 -0600

    add solution to Q309

--- Cpp/309_BestTimeToBuyAndSellStockWithCooldown/001.cpp
+++ Cpp/309_BestTimeToBuyAndSellStockWithCooldown/001.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int buy = INT_MIN;
+        int hold = INT_MIN;
         int sell = 0;
         int previousSell = 0;
         for (int price : prices) {
-            int previousBuy = buy;
-            buy = max(buy, previousSell - price);
+            int previousHold = hold;
+            hold = max(hold, previousSell - price);
             previousSell = sell;
-            sell = max(sell, previousBuy + price);
+            sell = max(sell, previousHold + price);
         }
         return sell;
     }
