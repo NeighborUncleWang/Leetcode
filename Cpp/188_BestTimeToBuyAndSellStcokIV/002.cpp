ffecd6445fab05e80f80678913dfe91e1c8a689c
Xianshu Li <lixianshu1992@gmail.com>
2015-12-24 21:34:29 -0600

    add comment to Q188 002.cpp

--- Cpp/188_BestTimeToBuyAndSellStcokIV/002.cpp
+++ Cpp/188_BestTimeToBuyAndSellStcokIV/002.cpp
@@ -12,6 +12,7 @@ public:
         vector<int> hold(k + 1, INT_MIN);
         vector<int> release(k + 1, 0);
         //下面两层for loop里面的两个赋值语句调换也能AC OJ
+        //即使j从0到k的顺序执行loop,交换两个赋值语句也能AC OJ
         for (int i = 0; i < pricesSize; ++i) {
             for (int j = k; j >= 0; --j) {
                 hold[j] = max(hold[j], release[j] - prices[i]);
