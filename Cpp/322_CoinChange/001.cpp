7454be2db7e75bdeefe3929efece7b65dbe07f4d
Xianshu Li <lixianshu1992@gmail.com>
2016-01-02 02:12:53 -0600

    add solution to Q322

---
+++ Cpp/322_CoinChange/001.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        //the amount + 1 is the largest number coin number 
+        //for an amount 
+        vector<int> dp(amount + 1, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; ++i) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp.back() > amount ? -1 : dp.back();
+    }
+};
---
+++ Cpp/322_CoinChange/readme.txt
@@ -0,0 +1,5 @@
+001.cpp:
+Time complexity: O(amount * coins.size())
+Space complexity: O(amount)
+DP solution, Reference:
+https://leetcode.com/discuss/76194/c-o-n-amount-time-o-amount-space-dp-solution
