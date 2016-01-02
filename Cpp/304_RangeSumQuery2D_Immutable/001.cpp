35684dfa299076c0149c358c3c73030838b8ead4
Xianshu Li <lixianshu1992@gmail.com>
2016-01-02 01:43:29 -0600

    add soluiton to Q304

---
+++ Cpp/304_RangeSumQuery2D_Immutable/001.cpp
@@ -0,0 +1,25 @@
+class NumMatrix {
+private:
+    vector<vector<int>> sums;
+public:
+    NumMatrix(vector<vector<int>> &matrix) {
+        int rowSize = matrix.size();
+        int columnSize = rowSize == 0 ? : matrix[0].size();
+        sums = vector<vector<int>>(rowSize + 1, vector<int>(columnSize + 1, 0));
+        for (int i = 0; i < rowSize; ++i) {
+            for (int j = 0; j < columnSize; ++j) {
+                sums[i + 1][j + 1] = sums[i + 1][j] + sums[i][j + 1] - sums[i][j] + matrix[i][j];
+            }
+        }
+    }
+
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return sums[row2 + 1][col2 + 1] + sums[row1][col1] - sums[row1][col2 + 1] - sums[row2 + 1][col1];
+    }
+};
+
+
+// Your NumMatrix object will be instantiated and called as such:
+// NumMatrix numMatrix(matrix);
+// numMatrix.sumRegion(0, 1, 2, 3);
+// numMatrix.sumRegion(1, 2, 3, 4);
---
+++ Cpp/304_RangeSumQuery2D_Immutable/readme.txt
@@ -0,0 +1,7 @@
+001.cpp:
+Time complexity:
+Construction: O(mn)
+Query: O(1)
+Space complexity: O(mn)
+Reference:
+https://leetcode.com/course/chapters/leetcode-101/range-sum-query-2d-immutable/
