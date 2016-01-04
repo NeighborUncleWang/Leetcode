571345ae1b4f05967ba999361ab2f35a17bb6469
Xianshu Li <lixianshu1992@gmail.com>
2016-01-03 22:08:02 -0600

    add solution to Q273

---
+++ Cpp/273_IntegerToEnglishWords/001.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string numberToWords(int num) {
+        const static vector<string> thousands{"", " Thousand", " Million", " Billion"};
+        int i = 0;
+        string words;
+        while (num) {
+            if (num % 1000) {
+                words = helper(num % 1000) + thousands[i] + words;
+            }
+            ++i;
+            num /= 1000;
+        }
+        return words.size() ? words.substr(1) : "Zero";
+    }
+private:
+    string helper(int num) {
+        const static vector<string> lessThan20{
+            "", " One", " Two", " Three", " Four", " Five", " Six", " Seven", " Eight", " Nine",
+            " Ten", " Eleven", " Twelve", " Thirteen", " Fourteen",  " Fifteen", " Sixteen", " Seventeen",
+            " Eighteen", " Nineteen"
+        };
+        const static vector<string> tens{
+            "", "", " Twenty", " Thirty", " Forty", " Fifty", " Sixty", " Seventy", " Eighty", " Ninety"
+        };
+        if (num == 0) {
+            return "";
+        } else if (num < 20) {
+            return lessThan20[num];
+        } else if (num < 100) {
+            return tens[num / 10] + lessThan20[num % 10];
+        } else {
+            return lessThan20[num / 100] + " Hundred" + helper(num % 100);
+        }
+    }
+};
---
+++ Cpp/273_IntegerToEnglishWords/readme.txt
@@ -0,0 +1,7 @@
+001.cpp:
+Time complexity: O(n)
+Space complexity: O(1)
+Reference:
+Combine the first reference with the solution of jianchao.li.fighter in second reference
+https://leetcode.com/discuss/55462/my-clean-java-solution-very-easy-to-understand
+https://leetcode.com/discuss/55273/my-java-solution
