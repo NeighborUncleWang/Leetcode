4ff8b0fc0f8cad294eb7d95a04a5b7d5fa6a52af
Xianshu Li <lixianshu1992@gmail.com>
2016-01-04 15:00:43 -0600

    add solution to Q277

---
+++ Cpp/277_FindTheCelebrity/001.cpp
@@ -0,0 +1,23 @@
+// Forward declaration of the knows API.
+bool knows(int a, int b);
+
+class Solution {
+public:
+    int findCelebrity(int n) {
+        if (n <= 1) return n - 1;
+        int candidate = 0;
+        for (int i = 1; i < n; ++i) {
+            if (knows(candidate, i)) {
+                candidate = i;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (i < candidate && (knows(candidate, i) || !knows(i, candidate))) {
+                return -1;
+            } else if (i > candidate && !knows(i, candidate)) {
+                return -1;
+            }
+        }
+        return candidate;
+    }
+};
---
+++ Cpp/277_FindTheCelebrity/readme.txt
@@ -0,0 +1,5 @@
+001.cpp:
+Time complexity: O(n)
+Space complexity: O(1)
+Reference:
+https://leetcode.com/discuss/56413/java-solution-two-pass
