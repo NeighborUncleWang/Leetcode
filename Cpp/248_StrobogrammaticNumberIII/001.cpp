b83dc059ed482fdbc4c1269728c8f41dcf24f6f9
Xianshu Li <lixianshu1992@gmail.com>
2016-01-11 00:58:00 -0600

    add solution to Q248

---
+++ Cpp/248_StrobogrammaticNumberIII/001.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    void dfs(string strobo, string& low, string& high, 
+    int left, int right, vector<string>& bases, int& count) {
+        if (left > right) {
+            if (strobo.size() == low.size() && strobo < low
+            || strobo.size() == high.size() && strobo > high) {
+                return;
+            } else {
+                ++count;
+                return;
+            }
+        }
+        for (string& base : bases) {
+            strobo[left] = base[0];
+            strobo[right] = base[1];
+            if (strobo.size() > 1 && strobo[0] == '0') {
+                continue;
+            }
+            if (left < right || left == right && base[0] == base[1]) {
+                dfs(strobo, low, high, left + 1, right - 1, bases, count);
+            }
+        }
+    }
+public:
+    int strobogrammaticInRange(string low, string high) {
+        vector<string> bases{"00", "11", "69", "88", "96"};
+        int count = 0;
+        for (int i = low.size(); i <= high.size(); ++i) {
+            dfs(string(i, '0'), low, high, 0, i - 1, bases, count);
+        }
+        return count;
+    }
+};
---
+++ Cpp/248_StrobogrammaticNumberIII/readme.txt
@@ -0,0 +1,7 @@
+001.cpp:
+Time complexity: O(?)
+Space complexity: O(?)
+Reference:
+https://leetcode.com/discuss/73721/easiest-20ms-94%25-java-solution
+Here is another solution from  jiaoliran(2nd answer, 1st answer can't pass the OJ any more), I haven't tried this:
+https://leetcode.com/discuss/55468/clear-java-ac-solution-using-strobogrammatic-number-method
