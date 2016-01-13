d326554cc87343da44da3fbf290b15be63da3760
Xianshu Li <lixianshu1992@gmail.com>
2016-01-12 21:11:20 -0600

    modify Q260

--- Cpp/260_SingleNumberIII/001.cpp
+++ Cpp/260_SingleNumberIII/001.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        vector<int> result(2, 0);
-        //using tailing-return-type for the lambda
-        //acutally it it optional since it can be deduced from the body of this lambda
-        int difference = accumulate(nums.begin(), nums.end(), 0, [](int lhs, int rhs) -> int {return lhs ^ rhs;});
-        //find the first 1 from the lowest bit
-        for (int i = 0; i < 32; ++i) {
-            if (difference & (1 << i)) {
-                difference &= (1 << i);
-                break;
-            }
-        }
-        //two other ways to get the first 1 from the lowest bit
-        //difference &= -difference;
-        //difference = (difference & (difference - 1)) ^ difference;
-        for (auto &i : nums) {
-            if (i & difference) {
-                result[0] ^= i;
+        int diff = accumulate(nums.begin(), nums.end(), 0, bit_xor<int>());
+        //diff & -diff can get the lowest 1 in diff
+        diff &= -diff;
+        vector<int> result{0, 0};
+        for (int num : nums) {
+            //==优先级比 & 高，记得加括号
+            if ((num & diff) == 0) {
+                result[0] ^= num;
             } else {
-                result[1] ^= i;
+                result[1] ^= num;
             }
         }
         return result;
--- Cpp/260_SingleNumberIII/readme.txt
+++ Cpp/260_SingleNumberIII/readme.txt
@@ -2,5 +2,6 @@
 Time complexity: O(n)
 Space complexity: O(1)
 Reference:
+https://leetcode.com/discuss/52351/accepted-java-space-easy-solution-with-detail-explanations
 http://www.cnblogs.com/grandyang/p/4741122.html
-https://leetcode.com/discuss/53150/solution-time-space-understaning-with-simple-explanation
+
