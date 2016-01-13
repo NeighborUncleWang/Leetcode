5a2af8d0c644e2c45674ed6bf50d5137138ad441
Xianshu Li <lixianshu1992@gmail.com>
2016-01-13 16:23:02 -0600

    modify Q191

--- Cpp/191_NumberOf1Bits/001.cpp
+++ Cpp/191_NumberOf1Bits/001.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int result = 0;
+        int count = 0;
         for (int i = 0; i < 32; ++i) {
-            if (n & 1) {
-                ++result;
-            }
-            //usigned 类型右移如果最高位是1会移进1吗？
-            //我认为应该不会才对，因为unsigned类型应该只有logic right shift
-            n >>= 1;
+            count += (n >> i) & 1;
         }
-        return result;
+        return count;
     }
 };
--- Cpp/191_NumberOf1Bits/readme.txt
+++ Cpp/191_NumberOf1Bits/readme.txt
@@ -1,4 +1,4 @@
 001.cpp:
 Time complexity: O(n)
 Space complexity: O(1)
-
+unsigned int 右移移入的都是0，int右移移入的是符号位。
