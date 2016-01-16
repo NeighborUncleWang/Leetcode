b66bb89e59d996c0bb64c66fd9cff4a8e69c366b
Xianshu Li <lixianshu1992@gmail.com>
2016-01-15 23:08:05 -0600

    modify solution to Q218

--- Cpp/218_TheSkylineProblem/002.cpp
+++ Cpp/218_TheSkylineProblem/002.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     vector<pair<int, int>> getSkyline(vector<vector<int>>& buildings) {
-        multiset<int> heights;
-        vector<pair<int, int>> xPositions;
-        vector<pair<int, int>> result;
+        vector<pair<int, int>> heights;
         //这里必须让左边界的高度值为负值，因为对pair1.first == pair2.first时，pair1.second < pair2.second才能保证
         //pair1排在pair2前面, 这是为了当前一栋建筑的右边界和后一栋建筑的左边界相等时，保证multiset里先进行插入操作后进行删除操作
         //这样能防止出现两次（x,0）,（x,height）的结果
@@ -14,23 +12,27 @@ public:
         //[[0,3],[2,0],[2,3],[5,0]]
         //Expected:
         //[[0,3],[5,0]]
-        for (auto &i : buildings) {
-            xPositions.push_back(pair<int, int>{i[0], -i[2]});
-            xPositions.push_back(pair<int, int>{i[1], i[2]});
+        for (vector<int>& building : buildings) {
+            heights.emplace_back(building[0], -building[2]);
+            heights.emplace_back(building[1], building[2]);
         }
-        sort(xPositions.begin(), xPositions.end());
-        heights.insert(0);
+        sort(heights.begin(), heights.end());
+        multiset<int, greater<int>> heap;
+        heap.insert(0);
         int previous = 0;
         int current = 0;
-        for (auto &i : xPositions) {
-            if (i.second < 0) {
-                heights.insert(-i.second);
+        vector<pair<int, int>> result;
+        for (auto& height : heights) {
+            if (height.second < 0) {
+                heap.insert(-height.second);
             } else {
-                heights.erase(heights.find(i.second));
+                //这里不要用heap.erase(height.second)
+                //否则会把所有value为height.second的元素都删除
+                heap.erase(heap.find(height.second));
             }
-            current = *heights.rbegin();
+            current = *heap.begin();
             if (current != previous) {
-                result.push_back(pair<int, int>{i.first, current});
+                result.emplace_back(height.first, current);
                 previous = current;
             }
         }
