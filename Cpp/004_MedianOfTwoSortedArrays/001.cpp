e649f375b99b3ea514fe15556552f84d33a24ebf
Xianshu Li <lixianshu1992@gmail.com>
2016-01-16 20:07:52 -0600

    modify 004.cpp

--- Cpp/004_MedianOfTwoSortedArrays/001.cpp
+++ Cpp/004_MedianOfTwoSortedArrays/001.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size();
-        int n = nums2.size();
-        if ((n + m) & 1 == 1) {
-            return helper(nums1, 0, m - 1, nums2, 0, n - 1, (m + n) / 2 + 1);
-        } else {
-            return (helper(nums1, 0, m - 1, nums2, 0, n - 1, (m + n) / 2)
-            + helper(nums1, 0, m - 1, nums2, 0, n - 1, (m + n) / 2 + 1)) * 1.0 / 2;
-        }
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        return (SplitMerge(nums1, 0, n1, nums2, 0, n2, (n1 + n2 - 1) / 2)
+                + SplitMerge(nums1, 0, n1, nums2, 0, n2, (n1 + n2) / 2)) / 2.0;
     }
-private:
-    double helper(vector<int>& nums1, int start1, int end1,
-    vector<int>& nums2, int start2, int end2, int k) {
-        int m = end1 - start1 + 1;
-        int n = end2 - start2 + 1;
-        //assume m <= n
-        if (m > n) {
-            return helper(nums2, start2, end2, nums1, start1, end1, k);
+    int SplitMerge(vector<int>& nums1, int start1, int end1, vector<int>& nums2, int start2, int end2, int k) {
+        int n1 = end1 - start1;
+        int n2 = end2 - start2;
+        if (n1 > n2) {
+            return SplitMerge(nums2, start2, end2, nums1, start1, end1, k);
         }
-        if (m == 0) {
-            return nums2[start2 + k - 1];
+        if (n1 == 0) {
+            return nums2[start2 + k];
         }
-        if (k == 1) {
+        if (k == 0) {
             return min(nums1[start1], nums2[start2]);
         }
-        int p1 = min(k / 2, m);
-        int p2 = k - p1;
-        if (nums1[start1 + p1 - 1] == nums2[start2 + p2 - 1]) {
-            return nums1[start1 + p1 - 1];
-        } else if (nums1[start1 + p1 - 1] < nums2[start2 + p2 - 1]) {
-            // remove the lower part of nums1 and upper part of nums2
-            //虽然移走两部分，但是k只受移走的lower part的影响，所以是k - p1 instead of k - p1 - p2
-            return helper(nums1, start1 + p1, end1, nums2, start2, start2 + p2 - 1, k - p1);
+        int p1 = min(k / 2, n1 - 1);
+        int p2 = k - p1 - 1;
+        if (nums1[start1 + p1] == nums2[start2 + p2]) {
+            return nums1[start1 + p1];
+        } else if (nums1[start1 + p1] < nums2[start2 + p2]) {
+            return SplitMerge(nums1, start1 + p1 + 1, end1, nums2, start2, end2, k - p1 - 1);
         } else {
-            // remove the lower part of nums2 and upper part of nums1
-            return helper(nums1, start1, start1 + p1, nums2, start2 + p2, end2, k - p2);
+            return SplitMerge(nums1, start1, end1, nums2, start2 + p2 + 1, end2, k - p2 - 1);
         }
     }
 };
