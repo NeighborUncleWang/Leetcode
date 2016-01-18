5712b2d599cb547f798b2c4aa0826e6f29bedb8c
Xianshu Li <lixianshu1992@gmail.com>
2016-01-17 21:34:12 -0600

    modify solution to Q228

--- Cpp/228_SummaryRanges/001.cpp
+++ Cpp/228_SummaryRanges/001.cpp
@@ -2,23 +2,15 @@ class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
         vector<string> result;
-        if (nums.size() == 0) {
-            return result;
-        }
-        int start = nums[0];
-        for (int i = 1; i < nums.size(); ++i) {
-            //这里不要用nums[i]- nums[i - 1] >= 2, 因为[-2147483647,2147483647]之间overflow之后gap变成负值，
-            //如果用>= 2条件判断会导致被当成一个连续的range
-            if (nums[i] - nums[i - 1] != 1) {
-                result.push_back(toString(start, nums[i - 1]));
-                start = nums[i];
+        for (int left = 0, right = 0; right < nums.size(); ++right) {
+            while (right < (int)nums.size() - 1 && nums[right + 1] <= nums[right] + 1) {
+                ++right;
             }
+            string temp = left == right ? to_string(nums[left]) 
+            : to_string(nums[left]) + "->" + to_string(nums[right]);
+            result.push_back(move(temp));
+            left = right + 1;
         }
-        result.push_back(toString(start, nums.back()));
         return result;
-    }
-private:
-    string toString(int start, int end) {
-        return start == end ? to_string(start) : to_string(start) + "->" + to_string(end);
     }
 };
--- Cpp/228_SummaryRanges/readme.txt
+++ Cpp/228_SummaryRanges/readme.txt
@@ -1,4 +1,5 @@
 001.cpp:
 Time complexity: O(n)
 Space complexity: O(1)
-
+Reference:
+https://leetcode.com/discuss/42229/10-line-c-easy-understand
