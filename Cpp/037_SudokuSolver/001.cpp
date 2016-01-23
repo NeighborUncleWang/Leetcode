9f7045e88a57b93c771d7991bfa37f7447a644f3
Xianshu Li <lixianshu1992@gmail.com>
2016-01-23 17:44:01 -0600

    .

--- Cpp/037_SudokuSolver/001.cpp
+++ Cpp/037_SudokuSolver/001.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     void solveSudoku(vector<vector<char>>& board) {
-        //actually don't need this, since the problem has mentioned that there is only one valid solution
-        if (board.size() == 0 || board[0].size() != 9) {
-            return;
-        }
-        helper(board, 0, 0);
+        dfs(board, 0, 0);
     }
 private:
-    bool helper(vector<vector<char>>& board, int row, int column) {
-        if (column == 9) {
-            return helper(board, row + 1, 0);
-        }
+    bool dfs(vector<vector<char>>& board, int row, int column) {
         if (row == 9) {
             return true;
+        } else if (column == 9) {
+            return dfs(board, row + 1, 0);
         }
         if (board[row][column] == '.') {
-            for (int i = 1; i <= 9; ++i) {
-                board[row][column] = i + '0';
-                if (isValid(board, row, column)) {
-                    if (helper(board, row, column + 1)) {
-                        return true;
-                    }
+            for (char ch = '1'; ch <= '9'; ++ch) {
+                board[row][column] = ch;
+                if (isValid(board, row, column) && dfs(board, row, column + 1)) {
+                    return true;
                 }
             }
-            board[row][column] = '.';//don't put this line into the for loop, 恢复现场
+            board[row][column] = '.';
+            return false;
         } else {
-            if (helper(board, row, column + 1)) {
-                return true;
-            }
+            return dfs(board, row, column + 1);
         }
-        return false;
     }
     bool isValid(vector<vector<char>>& board, int row, int column) {
         for (int i = 0; i < 9; ++i) {
@@ -38,13 +29,13 @@ private:
                 return false;
             }
         }
-        for (int i = 0; i < 9; ++i) {
-            if (i != column && board[row][i] == board[row][column]) {
+        for (int j = 0; j < 9; ++j) {
+            if (j != column && board[row][j] == board[row][column]) {
                 return false;
             }
         }
         for (int i = row / 3 * 3; i < row / 3 * 3 + 3; ++i) {
-            for (int j = column / 3 * 3; j < column / 3 * 3 + 3; ++j) {//I misuse j < row / 3 * 3 + 3, take more than 3 hrs to debug...
+            for (int j = column / 3 * 3; j < column / 3 * 3 + 3; ++j) {
                 if ((i != row || j != column) && board[i][j] == board[row][column]) {
                     return false;
                 }
--- Cpp/037_SudokuSolver/readme.txt
+++ Cpp/037_SudokuSolver/readme.txt
@@ -1,4 +1,8 @@
 001.cpp:
 Time complexity: O(9^81)
 Space complexity: O(1)
+Reference:
 http://blog.csdn.net/linhuanmars/article/details/20748761#reply
+
+Another solution（太长不看……）:
+https://leetcode.com/discuss/21452/sharing-my-2ms-c-solution-with-comments-and-explanations
