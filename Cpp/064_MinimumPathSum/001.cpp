3915b782e08f7909b97f7961e6fce030f19855c1
Xianshu Li <lixianshu1992@gmail.com>
2016-01-25 22:36:22 -0600

    .

--- Cpp/064_MinimumPathSum/001.cpp
+++ Cpp/064_MinimumPathSum/001.cpp
@@ -6,12 +6,9 @@ public:
         vector<int> result(column, INT_MAX);
         result[0] = 0;
         for (int i = 0; i < row; ++i) {
-            for (int j = 0; j < column; ++j) {
-                if (j == 0) {
-                    result[j] += grid[i][j];
-                } else {
-                    result[j] = grid[i][j] + min(result[j], result[j - 1]);
-                }
+            result[0] += grid[i][0];
+            for (int j = 1; j < column; ++j) {
+                result[j] = grid[i][j] + min(result[j], result[j - 1]);
             }
         }
         return result[column - 1];
