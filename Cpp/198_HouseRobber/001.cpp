9ff074181f1e9d707ba94380bf01e76d938eccd6
Xianshu Li <lixianshu1992@gmail.com>
2016-01-27 14:27:18 -0600

    add reference to Q198

--- Cpp/198_HouseRobber/001.cpp
+++ Cpp/198_HouseRobber/001.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int fn_2 = 0;
-        int fn_1 = 0;
-        int result = fn_1;
-        for (int i = 0; i < nums.size(); ++i) {
-            result = max(fn_2 + nums[i], fn_1);
-            fn_2 = fn_1;
-            fn_1 = result;
+        int robbed = 0;
+        int notRobbed = 0;
+        for (int num : nums) {
+            //use temp to store previous robbed value
+            int temp = robbed;
+            robbed = notRobbed + num;
+            notRobbed = max(temp, notRobbed);
         }
-        return result;
+        return max(robbed, notRobbed);
     }
 };
--- Cpp/198_HouseRobber/readme.txt
+++ Cpp/198_HouseRobber/readme.txt
@@ -1,6 +1,8 @@
 001.cpp:
 Time complexity: O(n)
 Space complexity: O(1)
+Reference:
+https://leetcode.com/discuss/31878/java-dp-solution-o-n-runtime-and-o-1-space-with-inline-comment
 002.cpp:
 Time complexity: O(n)
 Space complexity: O(1)
