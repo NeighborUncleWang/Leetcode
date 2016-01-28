d4875ca75cf1d3694a62ec83c57892bc6849af25
Xianshu Li <lixianshu1992@gmail.com>
2016-01-28 00:22:48 -0600

    add solution to Q115

--- Cpp/115_DistinctSubsequences/001.cpp
+++ Cpp/115_DistinctSubsequences/001.cpp
@@ -9,6 +9,8 @@ public:
             for (int j = n; j >= 1; --j) {
                 //记得在?:两边加（），否则会变成？result[j - 1] : 0 + result[j]
                 //或者写成result[j] += s[i - 1] == t[j - 1] ? result[j - 1] : 0;
+                //result[j - 1]代表选择当前t[j - 1]作为subsequence中的一个char,
+                //result[j]代表舍弃当前t[j - 1]
                 result[j] = (s[i - 1] == t[j - 1] ? result[j - 1] : 0) + result[j];
             }
         }
