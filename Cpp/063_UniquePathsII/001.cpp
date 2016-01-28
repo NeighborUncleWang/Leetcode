585881de6f82c29123aa23ba96482ca6fbbdba75
Xianshu Li <lixianshu1992@gmail.com>
2016-01-27 21:15:04 -0600

    add reference to Q0643

--- Cpp/063_UniquePathsII/001.cpp
+++ Cpp/063_UniquePathsII/001.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size();
-        int n = obstacleGrid[0].size();
-        vector<int> result(n, 0);
-        result[0] = 1;
-        //if obstacleGrid[0][0] == 1, then the fuction should return 0, since the robot can't start from Grid[0][0]
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
+        int row = obstacleGrid.size();
+        int column = row == 0 ? 0 : obstacleGrid[0].size();
+        vector<int> dp(column, 0);
+        //here we assume row and column both > 0
+        //we don't need to check whether obstacleGrid[0][0] == 1
+        //the code can handle this corner case
+        dp[0] = 1;
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < column; ++j) {
                 if (obstacleGrid[i][j] == 1) {
-                    result[j] = 0;
+                    dp[j] = 0;
                 } else if (j > 0) {
-                    result[j] += result[j - 1];
+                    dp[j] += dp[j - 1];
                 }
             }
         }
-        return result[n - 1];
+        return dp.back();
     }
 };
--- Cpp/063_UniquePathsII/readme.txt
+++ Cpp/063_UniquePathsII/readme.txt
@@ -1,4 +1,5 @@
 001.cpp:
 Time complexity: O(m * n)
 Space complexity: O(n)(can be O(min(m, n) if I want to rotate the matrix))
+https://leetcode.com/discuss/29816/short-java-solution
 http://blog.csdn.net/linhuanmars/article/details/22135231
