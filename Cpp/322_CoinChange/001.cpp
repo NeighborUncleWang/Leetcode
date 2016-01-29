3a43e496708b0a0ca3fed173f37d30af450b8170
Xianshu Li <lixianshu1992@gmail.com>
2016-01-28 19:10:22 -0600

    add comments to 001.cpp Q322

--- Cpp/322_CoinChange/001.cpp
+++ Cpp/322_CoinChange/001.cpp
@@ -3,6 +3,7 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         //the amount + 1 is the largest number coin number 
         //for an amount 
+        //we can use INT_MAX since it dp[i - coin] + 1 may overflow
         vector<int> dp(amount + 1, amount + 1);
         dp[0] = 0;
         for (int i = 1; i <= amount; ++i) {
