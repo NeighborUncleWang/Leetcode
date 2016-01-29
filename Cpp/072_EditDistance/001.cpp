f9ea3542d9f4a7c6662e6bf7237f22e57b86aec0
Xianshu Li <lixianshu1992@gmail.com>
2016-01-28 18:31:10 -0600

    modify 001.cpp to Q072

--- Cpp/072_EditDistance/001.cpp
+++ Cpp/072_EditDistance/001.cpp
@@ -4,22 +4,24 @@ public:
         if (word1.size() < word2.size()) {
             word1.swap(word2);
         }
-        vector<int> row(word2.size() + 1, 0);
-        for (int i = 0; i < row.size(); ++i) {
-            row[i] = i;
+        int row = word1.size() + 1;
+        int column = word2.size() + 1;
+        vector<int> previousRow(column, 0);
+        for (int i = 0; i < column; ++i) {
+            previousRow[i] = i;
         }
-        vector<int> nextRow(word2.size() + 1, 0);
-        for (int i = 1; i < word1.size() + 1; ++i) {
-            nextRow[0] = i;
-            for (int j = 1; j < word2.size() + 1; ++j) {
+        vector<int> currentRow(column, 0);
+        for (int i = 1; i < row; ++i) {
+            currentRow[0] = i;
+            for (int j = 1; j < column; ++j) {
                 if (word1[i - 1] == word2[j - 1]) {
-                    nextRow[j] = row[j - 1];//remember to assign row[j - 1] instead of row[j]
+                    currentRow[j] = previousRow[j - 1];
                 } else {
-                    nextRow[j] = min({row[j - 1], row[j], nextRow[j - 1]}) + 1;
+                    currentRow[j] = min({previousRow[j - 1], previousRow[j], currentRow[j - 1]}) + 1;
                 }
             }
-            row = nextRow;
+            previousRow = currentRow;
         }
-        return nextRow.back();
+        return currentRow.back();
     }
 };
