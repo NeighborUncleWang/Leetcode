85c0bf6f34b711257ea8c165af1ba8f8a6f765f5
Xianshu Li <lixianshu1992@gmail.com>
2016-01-28 19:16:37 -0600

    modify 001.cpp to Q303

--- Cpp/303_RangeSumQuery_Immutable/001.cpp
+++ Cpp/303_RangeSumQuery_Immutable/001.cpp
@@ -1,12 +1,13 @@
 class NumArray {
 private:
+    //accumulation[i] stores sum of [0, i)
     vector<int> accumulation;
 public:
     NumArray(vector<int> &nums) {
         int n = nums.size();
         accumulation = vector<int>(n + 1, 0);
-        for (int i = 1; i < n + 1; ++i) {
-            accumulation[i] = accumulation[i - 1] + nums[i - 1];
+        for (int i = 0; i < n; ++i) {
+            accumulation[i + 1] = accumulation[i] + nums[i];
         }
     }
 
