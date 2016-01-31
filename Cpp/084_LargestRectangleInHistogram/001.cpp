aae16773fb197723e6ef5831b53ad9b95b6f1d96
Xianshu Li <lixianshu1992@gmail.com>
2016-01-31 00:47:19 -0600

    add reference to Q084

--- Cpp/084_LargestRectangleInHistogram/001.cpp
+++ Cpp/084_LargestRectangleInHistogram/001.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int Max(int a, int b){return a > b ? a : b;}
     int largestRectangleArea(vector<int> &height) {
     	height.push_back(0);
-        stack<int> stk;
+        stack<int> indices;
         int i = 0;
         int maxArea = 0;
         while(i < height.size()){
-            if(stk.empty() || height[stk.top()] <= height[i]){
-                stk.push(i++);
+            if(indices.empty() || height[indices.top()] <= height[i]){
+                indices.push(i++);
             }else {
-                int t = stk.top();
-				stk.pop();
-                maxArea = Max(maxArea, height[t] * (stk.empty() ? i : i - stk.top() - 1));
+                int t = indices.top();
+				indices.pop();
+                maxArea = max(maxArea, height[t] * (indices.empty() ? i : i - indices.top() - 1));
             }
         }
         return maxArea;
--- Cpp/084_LargestRectangleInHistogram/readme.txt
+++ Cpp/084_LargestRectangleInHistogram/readme.txt
@@ -1,3 +1,7 @@
 Time complexity: O(n)
 Space complexity: O(n)
-The idea is from: http://blog.csdn.net/doc_sgl/article/details/11805519
+Reference:
+https://leetcode.com/discuss/12780/my-concise-c-solution-ac-90-ms
+A explanation with graph:
+这个示例图里有错，高度为1的柱子还有高度为2的柱子围成的矩形面积全都圈错了，看看就好，别被迷惑了
+http://blog.csdn.net/doc_sgl/article/details/11805519
