c013432e581ef793f1ad7edc9135c2f75325d3b4
Xianshu Li <lixianshu1992@gmail.com>
2016-01-31 22:47:07 -0600

    add reference to Q097

--- Cpp/097_InterleavingString/002.cpp
+++ Cpp/097_InterleavingString/002.cpp
@@ -7,20 +7,22 @@ public:
         if (m + n != k) {
             return false;
         }
-        vector<vector<bool>> matrix(m + 1, vector<bool>(n + 1, false));
-        matrix[0][0] = true;
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        dp[0][0] = true;
+        //initialize the first row
         for (int j = 1; j < n + 1; ++j) {
-            matrix[0][j] = s2[j - 1] == s3[j - 1] && matrix[0][j - 1];
-        }//initialize the first row
+            dp[0][j] = s2[j - 1] == s3[j - 1] && dp[0][j - 1];
+        }
+        //initialize the first column
         for (int i = 1; i < m + 1; ++i) {
-            matrix[i][0] = s1[i - 1] == s3[i - 1] && matrix[i - 1][0];
-        }//initialize the first column
+            dp[i][0] = s1[i - 1] == s3[i - 1] && dp[i - 1][0];
+        }
         for (int i = 1; i < m + 1; ++i) {
             for (int j = 1; j < n + 1; ++j) {
-                matrix[i][j] = matrix[i - 1][j] && s1[i - 1] == s3[i - 1 + j]
-                || matrix[i][j - 1] && s2[j - 1] == s3[i - 1 + j];
+                dp[i][j] = dp[i - 1][j] && s1[i - 1] == s3[i - 1 + j]
+                || dp[i][j - 1] && s2[j - 1] == s3[i - 1 + j];
             }
         }
-        return matrix[m][n];
+        return dp.back().back();
     }
 };
--- Cpp/097_InterleavingString/readme.txt
+++ Cpp/097_InterleavingString/readme.txt
@@ -5,3 +5,11 @@ Space complexity: O(min(m, n))
 Time complexity: O(m * n)
 Space complexity: O(m * n)
 002.cpp is the original naive version. 001.cpp is the improved one.
+Reference:
+https://leetcode.com/discuss/11694/my-dp-solution-in-c
+
+There is a BFS version:
+https://leetcode.com/discuss/19973/8ms-c-solution-using-bfs-with-explanation
+不过看不太懂，尤其不懂为什么要用unordered_map存储找过的点，BFS不是应该不会有重复找过的点的吗?
+想明白了，是因为(1,1)可以由(0,0)->(0,1)->(1,1)或者(0,0)->(1,0)->(1,1)得到,所以(1,1)有可能被
+重复找
