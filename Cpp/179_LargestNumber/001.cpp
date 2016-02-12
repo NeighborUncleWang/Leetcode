0b0897ccb25647d51f2e6ffb9d141c180e98b4cc
Xianshu Li <lixianshu1992@gmail.com>
2016-02-12 00:48:01 -0600

    add solution to Q179

--- Cpp/179_LargestNumber/001.cpp
+++ Cpp/179_LargestNumber/001.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end(), comparator());
+        int n = nums.size();
+        vector<string> numsStr(n);
+        transform(nums.begin(), nums.end(), numsStr.begin(), [](int num) {return to_string(num); });
+        sort(numsStr.begin(), numsStr.end(), [](string& lhs, string& rhs) {
+            return lhs + rhs > rhs + lhs;
+        });
         string result;
-        for (int i = 0; i < nums.size(); ++i) {
-            result += to_string(nums[i]);
-        }
-        auto start = result.find_first_not_of('0');
-        //trim the proceeding 0s
-        if (start == string::npos) {
+        result = accumulate(numsStr.begin(), numsStr.end(), result);
+        auto iter = result.find_first_not_of('0');
+        if (iter == string::npos) {
             return "0";
         } else {
-            return result.substr(start);
+            return result.substr(iter);
         }
     }
-private:
-    struct comparator {
-        bool operator()(int left, int right) {
-            string leftString = to_string(left);
-            string rightString = to_string(right);
-			//compare two strings instead of two int number to avoid overflow
-            return leftString + rightString > rightString + leftString;
-        }
-    };
 };
--- Cpp/179_LargestNumber/readme.txt
+++ Cpp/179_LargestNumber/readme.txt
@@ -1,5 +1,6 @@
 001.cpp:
 Time complexity: O(nlogn)
 Space complexity: O(1)
+https://leetcode.com/discuss/21683/a-simple-c-solution
 http://wlcoding.blogspot.com/2015/03/largest-number.html?view=sidebar
 http://bookshadow.com/weblog/2015/01/13/leetcode-largest-number/
