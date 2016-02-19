c58306f7811cb067603c0176f40500bba7e87645
Xianshu Li <lixianshu1992@gmail.com>
2016-02-19 17:00:21 -0600

    .

--- Cpp/117_PopulatingNextRightPointersInEachNodeII/001.cpp
+++ Cpp/117_PopulatingNextRightPointersInEachNodeII/001.cpp
@@ -9,35 +9,32 @@
 class Solution {
 public:
     void connect(TreeLinkNode *root) {
-        TreeLinkNode *currentLinkedListHead = nullptr;
-        TreeLinkNode *previousLevelCurrent = root;
-        TreeLinkNode *previousLevelHead = root;
-        TreeLinkNode *predecessor = nullptr;
-        while (previousLevelHead != nullptr) {//when previousLevelHead == nullptr, it means the whole tree is visited
-            previousLevelCurrent = previousLevelHead;
-            while (previousLevelCurrent != nullptr) {//when previousLevelCurrent == nullptr, it means this level is visited
-                if (previousLevelCurrent->left) {
-                    if (currentLinkedListHead == nullptr) {//check whether current node is the head of new level
-                        currentLinkedListHead = previousLevelCurrent->left;
-                        predecessor = currentLinkedListHead;
-                    } else {//current node is not the head of new level
-                        predecessor->next = previousLevelCurrent->left;
-                        predecessor = predecessor->next;
+        TreeLinkNode* head = nullptr;//head stores the first non-null element of next level
+        TreeLinkNode* predecessor = nullptr;//predecessor stores the last visited element of next level
+        TreeLinkNode* current = root;//current stores current element of current level
+        while (current) {
+            while (current) {
+                if (current->left) {
+                    if (predecessor) {
+                        predecessor->next = current->left;
+                    } else {
+                        head = current->left;
                     }
+                    predecessor = current->left;
                 }
-                if (previousLevelCurrent->right) {//current node is the head of new level
-                    if (currentLinkedListHead == nullptr) {
-                        currentLinkedListHead = previousLevelCurrent->right;
-                        predecessor = currentLinkedListHead;
-                    } else {//current node is not the head of new level
-                        predecessor->next = previousLevelCurrent->right;
-                        predecessor = predecessor->next;
+                if (current->right) {
+                    if (predecessor) {
+                        predecessor->next = current->right;
+                    } else {
+                        head = current->right;
                     }
+                    predecessor = current->right;
                 }
-                previousLevelCurrent = previousLevelCurrent->next;
+                current = current->next;
             }
-            previousLevelHead = currentLinkedListHead;
-            currentLinkedListHead = nullptr;
+            current = head;
+            head = nullptr;
+            predecessor = nullptr;
         }
     }
 };
--- Cpp/117_PopulatingNextRightPointersInEachNodeII/readme.txt
+++ Cpp/117_PopulatingNextRightPointersInEachNodeII/readme.txt
@@ -1,4 +1,5 @@
 001.cpp:
 Time complexity: O(n)
 Space complexity: O(1)
-
+Reference:
+https://leetcode.com/discuss/3339/o-1-space-o-n-complexity-iterative-solution
