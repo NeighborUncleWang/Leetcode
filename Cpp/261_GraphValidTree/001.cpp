d3c90de68201de5d375517502472f84bc75caaf9
Xianshu Li <lixianshu1992@gmail.com>
2016-02-19 18:52:41 -0600

    .

--- Cpp/261_GraphValidTree/001.cpp
+++ Cpp/261_GraphValidTree/001.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
     bool validTree(int n, vector<pair<int, int>>& edges) {
-        if (edges.size() != n - 1) {
+        if (n != edges.size() + 1) {
             return false;
         }
-        vector<int> parent(n);
-        for (int i = 0; i < n; ++i) {
-            parent[i] = i;
+        UnionFind unionFind(n);
+        for (auto& edge : edges) {
+            unionFind.unite(edge.first, edge.second);
         }
-        for (auto edge : edges) {
-            int rootFirst = getRoot(parent, edge.first);
-            int rootSecond = getRoot(parent, edge.second);
-            if (rootFirst == rootSecond) {
-                return false;
-            } else {
-                parent[rootFirst] = rootSecond; 
-            }
-        }
-        return true;
+        return unionFind.count == 1;
     }
 private:
-    int getRoot(vector<int>& parent, int index) {
-        while (parent[index] != index) {
-            index = parent[index];
+    struct UnionFind {
+        int count;
+        vector<int> roots;
+        vector<int> weights;
+        UnionFind(int n) {
+            roots = vector<int>(n, 0);
+            iota(roots.begin(), roots.end(), 0);
+            weights = vector<int>(n, 1);
+            count = n;
         }
-        return index;
-    }
+        int getRoot(int node) {
+            while (roots[node] != node) {
+                roots[node] = roots[roots[node]];
+                node = roots[node];
+            }
+            return node;
+        }
+        void unite(int left, int right) {
+            int leftRoot = getRoot(left);
+            int rightRoot = getRoot(right);
+            if (weights[leftRoot] < weights[rightRoot]) {
+                roots[leftRoot] = rightRoot;
+                weights[rightRoot] += weights[leftRoot];
+            } else {
+                roots[rightRoot] = leftRoot;
+                weights[leftRoot] += weights[rightRoot];
+            }
+            --count;
+        }
+    };
 };
