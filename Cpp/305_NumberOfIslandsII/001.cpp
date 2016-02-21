067a2204fbeb476d01043619868679e15108ef82
Xianshu Li <lixianshu1992@gmail.com>
2016-02-21 15:07:37 -0600

    .

--- Cpp/305_NumberOfIslandsII/001.cpp
+++ Cpp/305_NumberOfIslandsII/001.cpp
@@ -1,76 +1,72 @@
 class Solution {
 public:
     vector<int> numIslands2(int m, int n, vector<pair<int, int>>& positions) {
-        Union_Find islands(n);
-        vector<vector<int>> directions{{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        UnionFind unionFind(m, n);
+        vector<pair<int, int>> directions{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         vector<int> result;
         for (auto& position : positions) {
-            islands.add(position);
+            int i = position.first;
+            int j = position.second;
+            unionFind.add(i, j);
             for (auto& direction : directions) {
-                int newRowPosition = position.first + direction[0];
-                int newColumnPosition = position.second + direction[1];
+                int ii = i + direction.first;
+                int jj = j + direction.second;
                 //下面这行检查position是否valid必须有
                 //m=n=3, positions=[[0,1],[1,2],[2,1],[1,0],[0,2],[0,0],[1,1]]
                 //这个case过不去,因为当position=[0,2],direction=[0,1]时,[0,3]这个invalid position
                 //的index和[1,0]这个valid position的index一样,所以islands.contains返回true
                 //这会导致bug
-                if (newRowPosition < 0 || newRowPosition >= m
-                    || newColumnPosition < 0 || newColumnPosition >= n) continue;
-                auto newPosition = make_pair(newRowPosition, newColumnPosition);
-                if (islands.contains(newPosition)) {
-                    islands.unite(position, newPosition);
+                if (ii >= 0 && ii < m && jj >= 0 && jj < n && unionFind.roots[ii * n + jj] != -1) {
+                    unionFind.unite(i, j, ii, jj);
                 }
             }
-            result.push_back(islands.length());
+            result.push_back(unionFind.count);
         }
         return result;
     }
 private:
-    class Union_Find {
-    private:
-        unordered_map<long long, long long> id;
-        unordered_map<long long, int> size;
+    struct UnionFind {
         int count;
-        int columnSize;
-    public:
-        Union_Find(int columnSize) {
-            this->count = 0;
-            this->columnSize = columnSize;
+        vector<long long> weights;
+        vector<long long> roots;
+        int row;
+        int column;
+        UnionFind(int m, int n) {
+            count = 0;
+            weights = vector<long long>(m * n, 0);
+            roots = vector<long long>(m * n, -1);
+            row = m;
+            column = n;
         }
-        long long getIndex(pair<int, int> position) {
-            return position.first * columnSize + position.second;
+        long long getIndex(int i, int j) {
+            return i * column + j;
         }
-        int length() {
-            return count;
-        }
-        void add(const pair<int, int>& position) {
-            int index = getIndex(position);
-            id[index] = index;
-            size[index] = 1;
+        void add(int i, int j) {
+            long long index = getIndex(i, j);
+            roots[index] = index;
+            weights[index] = 1;
             ++count;
         }
-        bool contains(pair<int, int> position) {
-            long long index = getIndex(position);
-            return id.find(index) != id.end();
-        }
-        long long root(const pair<int, int>& position) {
-            long long index = getIndex(position);
-            while (id[index] != index) {
-                id[index] = id[id[index]];
-                index = id[index];
+        long long getRoot(int i, int j) {
+            long long index = getIndex(i, j);
+            while (index != roots[index]) {
+                roots[index] = roots[roots[index]];
+                index = roots[index];
             }
             return index;
         }
-        void unite(pair<int, int> lhs, pair<int, int> rhs) {
-            long long leftRoot = root(lhs);
-            long long rightRoot = root(rhs);
-            if (leftRoot == rightRoot) return;
-            if (size[leftRoot] < size[rightRoot]) {
-                id[leftRoot] = rightRoot;
-                size[rightRoot] += size[leftRoot];
+        void unite(int row1, int column1, int row2, int column2) {
+            long long leftRoot = getRoot(row1, column1);
+            long long rightRoot = getRoot(row2, column2);
+            if (leftRoot == rightRoot) {
+                return;
+            }
+            if (weights[leftRoot] < weights[rightRoot]) {
+                roots[leftRoot] = rightRoot;
+                weights[rightRoot] += weights[leftRoot];
             } else {
-                id[rightRoot] = leftRoot;
-                size[leftRoot] += size[rightRoot];
+                roots[rightRoot] = leftRoot;
+                weights[leftRoot] += weights[rightRoot];
             }
             --count;
         }
