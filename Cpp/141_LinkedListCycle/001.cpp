f513cc0149d154402f85efea25871d2592f39fec
Xianshu Li <lixianshu1992@gmail.com>
2016-02-22 23:45:39 -0600

    ,

--- Cpp/141_LinkedListCycle/001.cpp
+++ Cpp/141_LinkedListCycle/001.cpp
@@ -9,14 +9,15 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* runner = head;
-        ListNode* walker = head;
-        while (runner != nullptr && runner->next != nullptr) {
-            walker = walker->next;
-            runner = runner->next->next;
-            if (walker == runner) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
                 return true;
             }
+     
         }
         return false;
     }
--- Cpp/141_LinkedListCycle/readme.txt
+++ Cpp/141_LinkedListCycle/readme.txt
@@ -1,6 +1,3 @@
 001.cpp:
 Time complexity: O(n)
 Space complexity: O(1)
-002.cpp:
-Time complexity: O(n)
-Space complexity: O(n)
