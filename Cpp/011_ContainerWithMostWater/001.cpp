54dda09bb2d34b15841b378515e9680d06ee8835
Xianshu Li <lixianshu1992@gmail.com>
2016-02-23 19:56:44 -0600

    .

--- Cpp/011_ContainerWithMostWater/001.cpp
+++ Cpp/011_ContainerWithMostWater/001.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         int left = 0;
-        int right = height.size() - 1;
-        if (left >= right) {
-            return 0;
-        }
+        int right = (int)height.size() - 1;
         int max_area = 0;
         while (left < right) {
             max_area = max(max_area, (right - left) * min(height[left], height[right]));
--- Cpp/011_ContainerWithMostWater/readme.txt
+++ Cpp/011_ContainerWithMostWater/readme.txt
@@ -1,3 +1,5 @@
 Time complexity: O(n)
 Space complexity: O(1)
 Start from the biggest and smallest indices. If height[left] < height[right], then height[left] is the bottleneck, then ++low, and vice versa. Keep tracking the area of each container and find the biggest container.
+Reference:
+https://leetcode.com/discuss/11482/yet-another-way-to-see-what-happens-in-the-o-n-algorithm
