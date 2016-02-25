16cc94e26857bb30cb228aa4759411c754ccaec0
Xianshu Li <lixianshu1992@gmail.com>
2016-02-24 22:39:26 -0600

    .

--- Cpp/016_3SumClosest/001.cpp
+++ Cpp/016_3SumClosest/001.cpp
@@ -2,33 +2,22 @@ class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
-        int minDifference = INT_MAX;
-        for (vector<int>::size_type i = 0; i < nums.size() - 2; ++i) {
-            int currentDifference = twoSum(nums, target - nums[i], i + 1);
-            if (abs(currentDifference) < abs(minDifference)) {
-                minDifference = currentDifference;
+        int n = nums.size();
+        int closestSum = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < n - 2; ++i) {
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (abs(sum - target) < abs(closestSum - target)) {
+                    closestSum = sum;
+                    if (closestSum == target) {
+                        return closestSum;
+                    }
+                }
+                sum < target ? ++left : --right;
             }
         }
-        return target + minDifference;
-    }
-private:
-    int twoSum(vector<int>& nums, int target, vector<int>::size_type start) {
-        int minDifference = INT_MAX;
-        auto end = nums.size() - 1;
-        while (start < end) {
-            if (nums[start] + nums[end] == target) {
-                return 0;
-            }
-            int currentDifference = nums[start] + nums[end] - target;
-            if (abs(currentDifference) < abs(minDifference)) {
-                minDifference = currentDifference;
-            }
-            if (nums[start] + nums[end] > target) {
-                --end;
-            } else {
-                ++start;
-            }
-        }
-        return minDifference;
+        return closestSum;
     }
 };
--- Cpp/016_3SumClosest/readme.txt
+++ Cpp/016_3SumClosest/readme.txt
@@ -1,7 +1,5 @@
 001.cpp:
 Time complexity: O(n^2)
 Space complexity: O(1)
-002.cpp:
-Time complexity: O(n^2)
-Space complexity: O(1)
-This is a shorter version. The idea is the same.
+Reference:
+https://leetcode.com/discuss/6438/a-n-2-solution-can-we-do-better
