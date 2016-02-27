2ef7c9913c4ed32d57a8991199b972c287677e58
Xianshu Li <lixianshu1992@gmail.com>
2016-02-27 15:46:52 -0600

    Q030

--- Cpp/030_SubstringWithConcatenationOfAllWords/001.cpp
+++ Cpp/030_SubstringWithConcatenationOfAllWords/001.cpp
@@ -1,51 +1,47 @@
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
+        int n = s.size();
         int wordLength = words[0].size();
-        int wordsSize = words.size();
-        vector<int> result;
-        if (wordLength * wordsSize > s.size()) {
-            return result;
+        int count = 0;
+        int dictSize = words.size();
+        if (dictSize * wordLength > n) {
+            return vector<int>();
         }
-        unordered_map<string, int> needToFind;
+        unordered_map<string, int> toFind;
         unordered_map<string, int> hasFound;
-        for (int i = 0; i < words.size(); ++i) {
-            ++needToFind[words[i]];
+        for (string& s : words) {
+            ++toFind[s];
         }
+        vector<int> result;
         for (int i = 0; i < wordLength; ++i) {
+            int left = i;
             hasFound.clear();
-            int windowLeft = i;
-            int count = 0;
-            for (int windowRight = i; windowRight <= (int)s.size() - wordLength;
-            windowRight += wordLength) {
-                string currentWord = s.substr(windowRight, wordLength);
-                if (needToFind.find(currentWord) != needToFind.end()) {
+            count = 0;
+            for (int right = i; right < n - wordLength + 1; right += wordLength) {
+                string currentWord = s.substr(right, wordLength);
+                if (toFind.find(currentWord) != toFind.end()) {
                     ++hasFound[currentWord];
-                    if (hasFound[currentWord] <= needToFind[currentWord]) {
-                        ++count;
-                    } else {
-                        string temp = s.substr(windowLeft, wordLength);
-                        while (temp != currentWord) {
-                            --hasFound[temp];
-                            windowLeft += wordLength;
-                            --count;
-                            temp = s.substr(windowLeft, wordLength);
-                        }
+                    ++count;
+                    while (hasFound[currentWord] > toFind[currentWord]) {
+                        string temp = s.substr(left, wordLength);
                         --hasFound[temp];
-                        windowLeft += wordLength;
+                        --count;
+                        left += wordLength;
+                    }
+                    //at this point, the substring in [left, right] must be valid
+                    if (count == dictSize) {
+                        result.push_back(left);
+                        //even if I delete the following code, it can still pass the OJ
+                        //因为下一次loop会更新right和left
+                        --hasFound[s.substr(left, wordLength)];
+                        left += wordLength;
+                        --count;
                     }
                 } else {
-                    count = 0;
-                    windowLeft = windowRight + wordLength;
                     hasFound.clear();
-                }
-                if (count == words.size()) {
-                    result.push_back(windowLeft);
-                    //even if I delete the following code, it can still pass the OJ
-                    string temp = s.substr(windowLeft, wordLength);
-                    --hasFound[temp];
-                    windowLeft += wordLength;
-                    --count;
+                    count = 0;
+                    left = right + wordLength;
                 }
             }
         }
--- Cpp/030_SubstringWithConcatenationOfAllWords/readme.txt
+++ Cpp/030_SubstringWithConcatenationOfAllWords/readme.txt
@@ -2,11 +2,6 @@
 Time complexity: O(n)(n is the length of string s)
 Space complexity: O(l * m)(l is the length of word, m is the length of words)
 Use two pointer to maintain a window.
+Reference:
+https://leetcode.com/discuss/20151/an-o-n-solution-with-detailed-explanation
 http://blog.csdn.net/linhuanmars/article/details/20342851#reply
-This version is significantly faster than 002.cpp(52ms Vs 772ms)
-002.cpp:
-Time complexity: O(n - m + 1) * (l * m)
-Space complexity: O(l * m)
-A brute force solution:
-http://fisherlei.blogspot.com/2013/01/leetcode-substring-with-concatenation.html
-The soulmachine has a brute force version of this problem.
