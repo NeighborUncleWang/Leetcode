734f3f850f9eb26aec54b08f9e1bb467dbbb2ec8
Xianshu Li <lixianshu1992@gmail.com>
2016-02-27 14:51:58 -0600

    Q076

--- Cpp/076_MinimumWindowSubstring/001.cpp
+++ Cpp/076_MinimumWindowSubstring/001.cpp
@@ -1,40 +1,27 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char, string::size_type> expectCount;
-        for (string::size_type i = 0; i < t.size(); ++i) {
-            ++expectCount[t[i]];
+        vector<int> map(256, 0);
+        for (char ch : t) {
+            ++map[ch];
         }
-        unordered_map<char, string::size_type> realCount;
-        string::size_type left = 0;
-        string::size_type count = 0;
-        string::size_type minLength = numeric_limits<string::size_type>::max();
-        string::size_type minLeft = 0;
-        for (string::size_type right = 0; right < s.size(); ++right) {
-            if (expectCount.find(s[right]) != expectCount.end()) {
-                if (realCount[s[right]] < expectCount[s[right]]) {
-                    ++count;
-                }
-                ++realCount[s[right]];
+        int left = 0;
+        int right = 0;
+        int count = t.size();
+        int minLength = INT_MAX;
+        int minLeft = 0;
+        while (right < s.size()) {
+            if (--map[s[right++]] >= 0) {
+                --count;
             }
-            while (count == t.size()) {
-                if (right - left + 1 < minLength) {
+            while (count == 0) {
+                if (right - left < minLength) {
+                    minLength = right - left;
                     minLeft = left;
-                    minLength = right - left + 1;
                 }
-                if (expectCount.find(s[left]) != expectCount.end()) {//it means s[left] is in the expectCount, in the string t
-                    if (realCount[s[left]] <= expectCount[s[left]]) {
-                        --count;
-                    }
-                    --realCount[s[left]];
-                }
-                ++left;
+                if (++map[s[left++]] == 1) ++count;
             }
         }
-        if (minLength > s.size()) {
-            return "";
-        } else {
-            return s.substr(minLeft, minLength);
-        }
+        return minLength == INT_MAX ? "" : s.substr(minLeft, minLength);
     }
 };
