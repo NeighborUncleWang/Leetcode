ee2c74d2dbcccad06709584f678e954442416864
Xianshu Li <lixianshu1992@gmail.com>
2016-02-29 17:34:17 -0600

    .

--- Cpp/144_BinaryTreePreorderTraversal/002.cpp
+++ Cpp/144_BinaryTreePreorderTraversal/002.cpp
@@ -21,11 +21,13 @@ public:
                 while (predecessor->right != nullptr && predecessor->right != current) {
                     predecessor = predecessor->right;
                 }
-                if (predecessor->right == nullptr) {//2.a
+                //2.a这代表我们是第一次traverse到current node
+                //preorder traverse中在第一次traverse到current node时进行visit
+                if (predecessor->right == nullptr) {
                     result.push_back(current->val);
                     predecessor->right = current;
                     current = current->left;
-                } else {//2.b
+                } else {//2.b,这代表我们是第二次traverse到current节点
                     predecessor->right = nullptr;
                     current = current->right;
                 }
