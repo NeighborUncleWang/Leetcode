eeb146860d0ecba8422e3c8fc13f94658c0c3bef
Xianshu Li <lixianshu1992@gmail.com>
2016-03-04 16:06:44 -0600

    Q289

--- Cpp/289_GameOfLife/001.cpp
+++ Cpp/289_GameOfLife/001.cpp
@@ -5,15 +5,17 @@ public:
         int columnSize = rowSize ? board[0].size() : 0;
         for (int i = 0; i < rowSize; ++i) {
             for (int j = 0; j < columnSize; ++j) {
-                int count = -board[i][j];
+                //因为是计算neighbors的活着的数目，所以要除掉当前点
+                int lives = -(board[i][j] & 1);
                 for (int I = max(0, i - 1); I < min(i + 2, rowSize); ++I) {
                     for (int J = max(0, j - 1); J < min(j + 2, columnSize); ++J) {
                         //must be board[I][J] & 1 instead of board[I][J]
                         //since some board[I][J] may already be modified
-                        count += board[I][J] & 1;
+                        lives += board[I][J] & 1;
                     }
                 }
-                if (count == 3 || count + board[i][j] == 3) board[i][j] |= 2;
+                if (board[i][j] & 1 && lives == 2 || lives == 3) board[i][j] |= 2;
+                if (board[i][j] & 1 == 0 && lives == 3) board[i][j] |= 2;
             }
         }
         for (int i = 0; i < rowSize; ++i) {
--- Cpp/289_GameOfLife/readme.txt
+++ Cpp/289_GameOfLife/readme.txt
@@ -1,9 +1,5 @@
 001.cpp:
 Time complexity: O(n * m)
 Space complexity: O(1)
-https://leetcode.com/discuss/61912/c-o-1-space-o-mn-time
-002.cpp:
-Time complexity: O(n * m)
-Space complexity: O(1)
-https://leetcode.com/discuss/61910/clean-o-1-space-o-mn-time-java-solution
-001.cpp is a brilliant solution!
+Reference:
+https://leetcode.com/discuss/68352/easiest-java-solution-with-explanation
