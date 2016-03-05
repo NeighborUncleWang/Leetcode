229dec9944588f74ff610fbb8fbbc663954e35d2
Xianshu Li <lixianshu1992@gmail.com>
2016-03-05 17:05:10 -0600

    Q274

--- Cpp/274_H-Index/002.cpp
+++ Cpp/274_H-Index/002.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         int n = citations.size();
-        int h = 0;
         vector<int> counts(n + 1, 0);
         //当0 <= i < n时,counts[i]存着cite数==i的paper数量
         //当i == n时,counts[i]存着cite数>=n的paper数量
@@ -16,9 +15,11 @@ public:
                 ++counts[cites];
             }
         }
+        int result = 0;
         for (int i = n; i > 0; --i) {
-            h += counts[i];
-            if (h >= i) {
+            //result代表citation>=i的paper数量之和
+            result += counts[i];
+            if (result >= i) {
                 return i;
             }
         }
