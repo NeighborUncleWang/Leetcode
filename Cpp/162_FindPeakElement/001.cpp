e90b713bfe8719d0087276b92fd0244e86e27a34
Xianshu Li <lixianshu1992@gmail.com>
2016-03-04 22:10:59 -0600

    .

--- Cpp/162_FindPeakElement/001.cpp
+++ Cpp/162_FindPeakElement/001.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int n = nums.size();
-        int low = 0, high = nums.size() - 1;
-        if (n == 1) {
-            return 0;
-        } else if (nums[0] > nums[1]) {
-            return 0;
-        } else if (nums[n - 1] > nums[n - 2]) {
-            return n - 1;
-        } else {
-            while (low <= high) {
-                int median = low + (high - low) / 2;
-                if (nums[median] > nums[median - 1]
-                    && nums[median] > nums[median + 1]) {
-                    return median;
-                } else if (nums[median] < nums[median + 1]) {
-                    low = median + 1;
-                } else if (nums[median] < nums[median - 1]) {
-                    high = median - 1;
-                }
+        int size = nums.size();
+        int low = 0;
+        int high = size - 1;
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+            if (middle < size - 1 && nums[middle] < nums[middle + 1]) {
+                low = middle + 1;
+            } else {
+                high = middle - 1;
             }
-            return low;
         }
+        //low == size表示nums最后一段一直都是单调递增
+        //因为low是nums刚刚开始递减时的index，所以这种情况就越界了
+        //所以返回low - 1
+        return low == size ? low - 1 : low;
     }
 };
--- Cpp/162_FindPeakElement/readme.txt
+++ Cpp/162_FindPeakElement/readme.txt
@@ -2,6 +2,9 @@
 Time complexity: O(logn)
 Space compelxity: O(1)
 002.cpp:
-Time complexity: O(logn)
+Time complexity: O(n)
 Space complexity: O(1)
-The key idea is the same, but 002.cpp is shorter and doesn't need to consider conrner cases.
+Reference:
+https://leetcode.com/discuss/17793/find-the-maximum-by-binary-search-recursion-and-iteration?show=50529#a50529
+可以看看这段用导数(derivative/də'rɪvətɪv/)来解释的方法。
+D(-1) > 0 && D(n - 1) < 0，一定有一段是先增后减
