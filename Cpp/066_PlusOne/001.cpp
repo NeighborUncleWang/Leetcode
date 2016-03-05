77609bcb1bc499145367d4421a3923393dc20968
Xianshu Li <lixianshu1992@gmail.com>
2016-03-04 20:11:39 -0600

    Q066

--- Cpp/066_PlusOne/001.cpp
+++ Cpp/066_PlusOne/001.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int carry = 1;
-        auto n = digits.size();
-        vector<int> result(n);
+        int n = digits.size();
+        vector<int> result(n, 0);
         for (int i = n - 1; i >= 0; --i) {
             result[i] = (digits[i] + carry) % 10;
             carry = (digits[i] + carry) / 10;
--- Cpp/066_PlusOne/readme.txt
+++ Cpp/066_PlusOne/readme.txt
@@ -2,3 +2,5 @@
 Time complexity: O(n)
 Space compelxity: O(1)
 Use vector::insert(result.begin(), 1)
+Here is another solution do this in place:
+https://leetcode.com/discuss/14616/is-it-a-simple-code-c
