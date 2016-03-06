90a76b39f6038e94c54ffa8210c6e5cf8abdcf7e
Xianshu Li <lixianshu1992@gmail.com>
2016-03-06 15:40:32 -0600

    Q146

--- Cpp/146_LRUCache/001.cpp
+++ Cpp/146_LRUCache/001.cpp
@@ -1,50 +1,41 @@
 class LRUCache{
+private:
+    unordered_map<int, list<pair<int, int>>::iterator> hashMap;
+    list<pair<int, int>> keyList;
+    int capacity;
+    void moveToHead(int key) {
+        keyList.emplace_front(key, hashMap[key]->second);
+        keyList.erase(hashMap[key]);
+        hashMap[key] = keyList.begin();
+    }
 public:
     LRUCache(int capacity) {
         this->capacity = capacity;
     }
-    
+    //each time we set, insert, modify an element, we move it to the head of the list
+    //the doubly-linked list is the data structure actually store the cache nodes, 
+    //the hashMap maps the iterator of the list with the key
     int get(int key) {
-        if (hashMap.find(key) == hashMap.end()) {
-            return -1;
+        if (hashMap.find(key) != hashMap.end()) {
+            moveToHead(key);
+            return hashMap[key]->second;
         } else {
-            MoveToHead(key);
-            return hashMap[key]->value;
+            return -1;
         }
     }
     
     void set(int key, int value) {
-        //each time we set, insert, modify an element, we move it to the head of the list
-        //the doubly-linked list is the data structure actually store the cache nodes, 
-        //the hashMap maps the iterator of the list with the key
         if (hashMap.find(key) != hashMap.end()) {
-            hashMap[key]->value = value;
-            MoveToHead(key);
+            hashMap[key]->second = value;
+            moveToHead(key);
         } else {
             //delete the element at the tail of the list, that is the LRU element
-            if (cacheList.size() == capacity) {
-                hashMap.erase(cacheList.back().key);
-                cacheList.pop_back();
-                cacheList.push_front(CacheEntry(key, value));
-                hashMap[key] = cacheList.begin();
-            } else {
-                cacheList.push_front(CacheEntry(key, value));
-                hashMap[key] = cacheList.begin();
+            if (hashMap.size() == capacity) {
+                hashMap.erase(keyList.back().first);
+                keyList.pop_back();
             }
+            keyList.emplace_front(key, value);
+            hashMap[key] = keyList.begin(); 
         }
-    }
-private:
-    struct CacheEntry{
-        int key;
-        int value;
-        CacheEntry(int key, int value) : key(key), value(value) {}
-    };
-    int capacity;
-    list<CacheEntry> cacheList;
-    unordered_map<int, list<CacheEntry>::iterator> hashMap;
-    void MoveToHead(int key) {
-        cacheList.push_front(*hashMap[key]);
-        cacheList.erase(hashMap[key]);
-        hashMap[key] = cacheList.begin();
     }
 };
--- Cpp/146_LRUCache/readme.txt
+++ Cpp/146_LRUCache/readme.txt
@@ -1,4 +1,5 @@
 001.cpp:
 Time complexity: O(1)
 Space complexity: O(n)
+Reference:
 http://fisherlei.blogspot.com/2013/11/leetcode-lru-cache-solution.html
