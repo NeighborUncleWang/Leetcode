6187246c3b3388e6d11c7ae84dc37f6201adf178
Xianshu Li <lixianshu1992@gmail.com>
2016-03-05 20:24:50 -0600

    Q275

--- Cpp/275_H-IndexII/001.cpp
+++ Cpp/275_H-IndexII/001.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        int n = citations.size();
+        int size = citations.size();
         int low = 0;
-        int high = n - 1;
-        int mid = 0;
+        int high = size - 1;
         while (low <= high) {
-            mid = low + (high - low) / 2;
-            if (citations[mid] == n - mid) {
-                return n - mid;
-            } else if (citations[mid] > n - mid) {
-                high = mid - 1;
+            int middle = low + (high - low) / 2;
+            //如果size - middle是一个valid H-index
+            //那么表示citations[middle] >= size - middle
+            //因为表示有size - middle篇论文citations>= size - middle
+            //因为size - middle篇论文citations都>=citations[middle]
+            //所以citations[middle]>= size - middle
+            //这表明size - middle可以是一个valid h-index
+            //然后我们让middle变小，来看是否能增大h-index
+            //最后一定是low和high两个index分别满足citations[index] >= size - index
+            //和citations[index] < size - index
+            //然后根据把citations画成图像可以知道index越大越容易满足citations[index] >= size - index
+            //所以一定是citations[low] >= size - low
+            //citations[high] < size - high
+            if (citations[middle] >= size - middle) {
+                high = middle - 1;
             } else {
-                low = mid + 1;
+                low = middle + 1;
             }
         }
-        //之所以选n - low 是因为low = high + 1
-        //citations[low] >= n - low
-        //citations[high] < n - high
-        //low和high一定是一个让citations[index] >= n - index 满足
-        //另一个让这个条件不满足。因为更大的index更容易满足上面的条件
-        //所以应该是low满足条件，所以返回n - low
-        return n - low;
+        return size - low;
     }
 };
--- Cpp/275_H-IndexII/readme.txt
+++ Cpp/275_H-IndexII/readme.txt
@@ -1,4 +1,5 @@
 001.cpp:
 Time complexity: O(logn)
 Space complexity: O(1)
-http://bookshadow.com/weblog/2015/09/04/leetcode-h-index-ii/
+https://leetcode.com/discuss/56122/standard-binary-search
+https://leetcode.com/discuss/58120/share-my-c-o-logn-solution-with-explanation
