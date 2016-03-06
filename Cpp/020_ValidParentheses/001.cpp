0692af6a175f6b2225779844d4e03186262d8fd5
Xianshu Li <lixianshu1992@gmail.com>
2016-03-05 20:31:14 -0600

    Q020

--- Cpp/020_ValidParentheses/001.cpp
+++ Cpp/020_ValidParentheses/001.cpp
@@ -2,26 +2,21 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        for (auto ch : s) {
+        for (char ch : s) {
             if (ch == '(' || ch == '{' || ch == '[') {
                 st.push(ch);
             } else {
                 if (st.empty()) {
                     return false;
                 }
-                auto top = st.top();
-                if (top == '(' && ch == ')' || top == '{' && ch == '}'
-                || top == '[' && ch == ']') {
-                    st.pop();
-                } else {
+                char temp = st.top();
+                st.pop();
+                if (ch == ')' && temp != '(' || ch == '{' && temp != '}'
+                || ch == '[' && temp != ']') {
                     return false;
                 }
             }
         }
-        if (st.empty()) {
-            return true;
-        } else {
-            return false;
-        }
+        return st.empty();
     }
 };
