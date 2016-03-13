d991424786067b450075a163e72db95ddb81f4db
Xianshu Li <lixianshu1992@gmail.com>
2016-03-13 16:48:47 -0500

    Q035

--- Cpp/035_SearchInsertPosition/001.cpp
+++ Cpp/035_SearchInsertPosition/001.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        return binarySearch(nums, 0, nums.size() - 1, target);
-    }
-private:
-    int binarySearch(vector<int>& nums, int low, int high, int target) {
-        if (low > high) {
-            return low;
+        int low = 0;
+        int high = (int)nums.size() - 1;
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+            if (nums[middle] == target) {
+                return middle;
+            } else if (nums[middle] < target) {
+                low = middle + 1;
+            } else {
+                high = middle - 1;
+            }
         }
-        int median = (low + high) / 2;
-        if (nums[median] == target)
-            return median;
-        else if (nums[median] > target)
-            return binarySearch(nums, low, median - 1, target);
-        else 
-            return binarySearch(nums, median + 1, high, target);
+        return low;
     }
 };
---
+++ Cpp/035_SearchInsertPosition/readme.txt
@@ -0,0 +1,5 @@
+001.cpp:
+Time complexity: O(logn)
+Space complexity: O(1)
+Reference:
+https://leetcode.com/discuss/39829/c-o-logn-binary-search-that-handles-duplicate
