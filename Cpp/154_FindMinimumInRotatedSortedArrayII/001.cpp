cf7c83337f646b4a27754af81a4cc727386b2ca0
Xianshu Li <lixianshu1992@gmail.com>
2016-03-14 02:12:28 -0500

    Q

--- Cpp/154_FindMinimumInRotatedSortedArrayII/001.cpp
+++ Cpp/154_FindMinimumInRotatedSortedArrayII/001.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int low = 0, high = nums.size() - 1, mid;
+        int size = nums.size();
+        int low = 0;
+        int high = size - 1;
         while (low < high) {
-            mid = low + (high - low) / 2;
-            if (nums[mid] > nums[high]) {
-                low = mid + 1;
-            } else if (nums[mid] < nums[high]) {
-                high = mid;
+            int middle = low + (high - low) / 2;
+            if (nums[middle] > nums[high]) {
+                low = middle + 1;
+            } else if (nums[middle] < nums[high]) {
+                high = middle;
             } else {
                 --high;
             }
--- Cpp/154_FindMinimumInRotatedSortedArrayII/readme.txt
+++ Cpp/154_FindMinimumInRotatedSortedArrayII/readme.txt
@@ -1,3 +1,8 @@
+001.cpp:
 Time complexity: O(n)
 Space complexity: O(1)
-if nums[mid] == nums[high], then --high.
+Reference:
+https://leetcode.com/discuss/19746/my-pretty-simple-code-to-solve-it
+这题我完全不懂怎么回事。首先discussion里的答案和153. Find Minimum in Rotated Sorted Array discussion里的一样，用的都是wikipedia上Deferred detection of equality的binary search.
+还有为什么nums[middle] > nums[high]能消掉一半我也不理解（好像理解了，nums[middle] > nums[high]）最小值只能在右边，但是为什么正常的binary search（low <= high, high = middle + 1）版本又不能工作，我又不懂了。
+这题其实worst case已经是O(n)了，TMD用毛binary search，直接return *min_element(nums.begin(), nums.end())算了。
