b06241625200137eeafa4315025c247581756748
Xianshu Li <lixianshu1992@gmail.com>
2016-03-15 00:07:54 -0500

    Q119

--- Cpp/119_Pascal'sTriangleII/001.cpp
+++ Cpp/119_Pascal'sTriangleII/001.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<int> result(rowIndex + 1, 0);
-        for (int i = 0; i <= rowIndex; ++i) {
-            for (int j = i; j >= 0; --j) {
-                if (j == 0) {
-                    result[j] = 1;
-                } else {
-                    result[j] = result[j] + result[j - 1];
-                }
+        vector<int> result(rowIndex + 1, 1);
+        for (int i = 0; i < rowIndex + 1; ++i) {
+            for (int j = i - 1; j > 0; --j) {
+                //可以写成result[j] += result[j - 1];
+                result[j] = result[j] + result[j - 1];
             }
         }
         return result;
--- Cpp/119_Pascal'sTriangleII/readme.txt
+++ Cpp/119_Pascal'sTriangleII/readme.txt
@@ -1,3 +1,5 @@
 Time complexity: O(n2)
 Space complexity: O(n)
-Use only one vector to store the result. Remember to calculate the element in the vector from back to front so that the previous value will not be eliminated when you need to use it. 001.cpp is just 2 lines shorter than 002.cpp
+Use only one vector to store the result. Remember to calculate the element in the vector from back to front so that the previous value will not be eliminated when you need to use it.
+Reference:
+https://leetcode.com/discuss/8364/here-is-my-brief-o-k-solution
