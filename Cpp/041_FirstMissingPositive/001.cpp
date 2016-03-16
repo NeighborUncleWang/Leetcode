e44b293f5c2bac30cf17a7b472814233b0265bd1
Xianshu Li <lixianshu1992@gmail.com>
2016-03-15 19:18:11 -0500

    Q041

--- Cpp/041_FirstMissingPositive/001.cpp
+++ Cpp/041_FirstMissingPositive/001.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int n = nums.size();
-        for (int i = 0; i < n; ++i) {
-            while (nums[i] > 0 && nums[i] <= n
-                   && nums[i] != nums[nums[i] - 1]) {
-				swap(nums[i], nums[nums[i] - 1]);
+        int size = nums.size();
+        for (int i = 0; i < size; ++i) {
+            //因为nums[i]swap完之后还要判断新的value是否满足条件
+            //所以要用while语句
+            //nums[nums[i] - 1]是看nums[i]应该在的位置有没有被nums[i]填充
+            //不能判断nums[i] != i + 1
+            //因为有可能整个nums不存在i + 1value
+            //然后就死循环了
+            while (nums[i] > 0 && nums[i] <= size
+            && nums[nums[i] - 1] != nums[i]) {
+                swap(nums[nums[i] - 1], nums[i]);
             }
         }
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < size; ++i) {
             if (nums[i] != i + 1) {
                 return i + 1;
             }
         }
-        return n + 1;
+        return size + 1;
     }
 };
---
+++ Cpp/041_FirstMissingPositive/readme.txt
@@ -0,0 +1,6 @@
+001.cpp:
+Time complexity: O(n)
+Space complexity: O(1)
+Reference:
+https://leetcode.com/discuss/24013/my-short-c-solution-o-1-space-and-o-n-time
+http://blog.csdn.net/linhuanmars/article/details/20884585
