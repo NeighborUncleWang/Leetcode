ad21a6d17d59db7cebe982e0a36b53442866af90
Xianshu Li <lixianshu1992@gmail.com>
2016-03-16 19:46:43 -0500

    Q170

--- Cpp/170_TwoSumIII-DataStructureDesign/001.cpp
+++ Cpp/170_TwoSumIII-DataStructureDesign/001.cpp
@@ -1,28 +1,28 @@
 class TwoSum {
+private:
+    unordered_map<int, int> hashMap;
 public:
+
+    // Add the number to an internal data structure.
 	void add(int number) {
 	    ++hashMap[number];
 	}
 
+    // Find if there exists any pair of numbers which sum is equal to the value.
 	bool find(int value) {
-	    for (auto it = hashMap.begin(); it != hashMap.end(); ++it) {
-	        auto target = value - it->first;
-	        //先检查target是否等于it->first，而不是temp = hashMap.find(target)之后再来看temp == it
-	        //这样可以省下一些情况的搜索时间，否则会TLE
-			//其实即使AC也非常接近TLE了（1540ms）
-			//有时候再次提交会TLE，这网站坑爹啊
-	        if (target == it->first) {
-	            if (it->second > 1) {
-	                return true;
-	            }
-	        } else {
-	            if (hashMap.find(target) != hashMap.end()) {
-	                return true;
-	            }
+	    for (auto iter : hashMap) {
+	        int target = value - iter.first;
+	        if (target == iter.first && iter.second > 1 
+	        || target != iter.first && hashMap.find(target) != hashMap.end()) {
+	            return true;
 	        }
 	    }
 	    return false;
 	}
-private:
-    unordered_map<int, int> hashMap;
 };
+
+
+// Your TwoSum object will be instantiated and called as such:
+// TwoSum twoSum;
+// twoSum.add(number);
+// twoSum.find(value);
