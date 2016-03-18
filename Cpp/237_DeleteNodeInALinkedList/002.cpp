fc5c96dc44f34a798d26e1f2198494838ec263bf
Xianshu Li <lixianshu1992@gmail.com>
2016-03-17 22:09:10 -0500

    Q237

--- Cpp/237_DeleteNodeInALinkedList/002.cpp
+++ Cpp/237_DeleteNodeInALinkedList/002.cpp
@@ -9,8 +9,9 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        auto next = node->next;
-        *node = *next;
-        delete next;
+        swap(node->val, node->next->val);
+        auto temp = node->next;
+        node->next = node->next->next;
+        delete temp;
     }
 };
--- Cpp/237_DeleteNodeInALinkedList/readme.txt
+++ Cpp/237_DeleteNodeInALinkedList/readme.txt
@@ -1,4 +1,9 @@
 001.cpp:
 Time complexity: O(1)
 Space complexity: O(1)
-
+Reference:
+https://leetcode.com/discuss/45851/1-3-lines-c-java-python-c-c%23-javascript-ruby
+002.cpp:
+Time complexity: O(1)
+Space complexity: O(1)
+My solution.
