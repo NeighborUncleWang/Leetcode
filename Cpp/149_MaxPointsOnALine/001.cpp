43310b5745467b3fdda1550e87878e7c567a16db
Xianshu Li <lixianshu1992@gmail.com>
2016-03-17 21:49:07 -0500

    Q149

--- Cpp/149_MaxPointsOnALine/001.cpp
+++ Cpp/149_MaxPointsOnALine/001.cpp
@@ -10,24 +10,30 @@
 class Solution {
 public:
     int maxPoints(vector<Point>& points) {
+        int size = points.size();
+        if (size == 0) return 0;
         unordered_map<double, int> hashMap;
-        //当points.size() == 1时，下面的for loop不会执行，所以这里maxNumber = 1保证函数最后还能返回1
-        int maxNumber = 1;
-        if (points.size() == 0) {
-            return 0;
-        }
-        //必须单独考虑points.size() == 0情况，否则下面points.size() - 1返回的不是-1而是size_t::max，导致死循环
-        //另一种方法是i < (int)points.size()
-        for (int i = 0; i < points.size() - 1; ++i) {
-            int duplicate = 1;
-            double slope = 0.0;
+        //当points.size() == 1时，下面的for loop不会执行，所以这里maxPoints = 1保证函数最后还能返回1
+        int maxPoints = 1;
+        for (int i = 0; i < size - 1; ++i) {
+            //我一开始写的时候没有写hashMap.clear()
+            //相当于把所有的可能的pair都存到同一个hashMap里了
+            //这样会有很多重复
+            //但是我也不是很懂为什么每次清空hashMap就是正确的
+            //因为一个固定经过的点和斜率决定一系列直线，如果两条线平行只按斜率来存到hashMap里明显就不对了
+            //但是这个方法还是有很多重复计算，比如所有点都不相同然后都共线，
+            //那么其实只要outer loop执行一次就得到正确结果了，但是这里还是会让outer loop执行size - 1次
             hashMap.clear();
-            //这里是考虑所有点都是duplicate的情况，如果不加入这一项会导致hashMap为空，后面计算maxNumber的for loop也无法执行，从而出错，另一种方法是设一个localMax
-            hashMap[numeric_limits<double>::lowest()] = 0;
-            for (int j = i + 1; j < points.size(); ++j) {
+            int duplicates = 1;
+            int localMax = 0;
+            //这里是考虑所有点都是duplicate的情况，如果不加入这一项会导致hashMap为空，
+            //后面计算maxNumber的for loop也无法执行，从而出错，另一种方法是设一个localMax
+            for (int j = i + 1; j < size; ++j) {
                 if (points[i].x == points[j].x && points[i].y == points[j].y) {
-                    ++duplicate;
+                    //这里要考虑duplicates
+                    ++duplicates;
                 } else {
+                    double slope;
                     if (points[i].x == points[j].x) {
                         slope = numeric_limits<double>::max();
                     } else {
@@ -36,10 +42,11 @@ public:
                     ++hashMap[slope];
                 }
             }
-            for (auto it : hashMap) {
-                maxNumber = max(maxNumber, it.second + duplicate);
+            for (auto& iter : hashMap) {
+                localMax = max(localMax, iter.second);
             }
+            maxPoints = max(maxPoints, localMax + duplicates);
         }
-        return maxNumber;
+        return maxPoints;
     }
 };
--- Cpp/149_MaxPointsOnALine/readme.txt
+++ Cpp/149_MaxPointsOnALine/readme.txt
@@ -1,6 +1,9 @@
 001.cpp:
 Time complexity: O(n^2)
 Space complexity: O(n)
-002.cpp:
-Time complexity: O(n^2)
-Space complexity: O(n)
+Reference:
+https://leetcode.com/discuss/18590/sharing-my-simple-solution-with-explanation
+我感觉discussion里其他高票答案不对，有个答案用map<pair<int, int>, int>来存线，但是每次insert的complexity是O(logn)
+reeclapple的方法应该将map<map<int, int>, int>做容器，而不是map<int, map<int, int>>，前者会更好理解(一对a和b决定斜率作为key)。
+一旦用了map复杂度就上升，不用map的话pair<int,int>又得自己想hash function，评论里有一个方法，得到int a = xdiff/gcd和int b = ydiff/gcd之后,
+用long long key = a << 32 | b来得到key，这个方法还不错的样子。
