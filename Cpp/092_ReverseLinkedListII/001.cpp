6dc5f27dd58e6d63cb19ac5965376d54782b86eb
Xianshu Li <lixianshu1992@gmail.com>
2016-03-18 15:22:06 -0500

    Q092

--- Cpp/092_ReverseLinkedListII/001.cpp
+++ Cpp/092_ReverseLinkedListII/001.cpp
@@ -9,20 +9,19 @@
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int m, int n) {
-        auto dummy = new ListNode(0);
+        ListNode dummyNode(0);
+        ListNode* dummy = &dummyNode;
         dummy->next = head;
         auto predecessor = dummy;
-        for (int i = 1; i < m; ++i) {
+        for (int i = 0; i < m - 1; ++i) {
             predecessor = predecessor->next;
         }
-        auto mNode = predecessor->next;
-        auto current = mNode->next;
-        for (int i = m; i < n; ++i) {
-            auto next = current->next;
-            current->next = predecessor->next;
-            predecessor->next = current;
-            mNode->next = next;
-            current = next;
+        auto start = predecessor->next;
+        for (int i = 0; i < n - m; ++i) {
+            auto then = start->next;
+            start->next = then->next;
+            then->next = predecessor->next;
+            predecessor->next = then;
         }
         return dummy->next;
     }
--- Cpp/092_ReverseLinkedListII/readme.txt
+++ Cpp/092_ReverseLinkedListII/readme.txt
@@ -1,8 +1,6 @@
 001.cpp:
 Time complexity: O(n)
 Space complexity: O(1)
-The most concise version.
-002.cpp:
-Time cpmlexity: O(n)
-Space complexity: O(1)
-Add some defensive code for corner cases.
+Reference:
+https://leetcode.com/discuss/25580/simple-java-solution-with-clear-explanation
+思路是找到要reverse的range里的第一个node标为start，start的前置节点标为predecessor，然后每次把start后一个节点then插入到predecessor和predecessor->next之间。整个过程predecessor指向的节点不变，predecessor指向节点的位置也不变。start指向的节点不变，start指向的节点的位置不断地后移(因为start->next不断被插入到predecessor和predecessor->next之间)
