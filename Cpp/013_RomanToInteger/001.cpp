225e9bdcfd9d3b87cccdd4dfe026aaff7f6f3d1b
Xianshu Li <lixianshu1992@gmail.com>
2016-03-19 15:45:35 -0500

    Q013

--- Cpp/013_RomanToInteger/001.cpp
+++ Cpp/013_RomanToInteger/001.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> dictionary{
-            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
-            {'C', 100}, {'D', 500}, {'M', 1000}
-        };
+        unordered_map<char, int> hashMap{{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+        {'C', 100}, {'D', 500}, {'M', 1000}};
         int result = 0;
-        for (int i = 0; i < s.size(); ++i) {
-            if (i < s.size() - 1 && dictionary[s[i]] < dictionary[s[i + 1]]) {
-                result -= 2 * dictionary[s[i]];
+        int size = s.size();
+        for (int i = 0; i < size; ++i) {
+            if (i < size - 1 && hashMap[s[i]] < hashMap[s[i + 1]]) {
+                result -= hashMap[s[i]];
+            } else {
+                result += hashMap[s[i]];
             }
-            result += dictionary[s[i]];
         }
         return result;
     }
