3245076436cfc3b422b428e0c8b05150205d2691
Xianshu Li <lixianshu1992@gmail.com>
2016-03-19 22:32:36 -0500

    Q067

--- Cpp/067_AddBinary/002.cpp
+++ Cpp/067_AddBinary/002.cpp
@@ -5,7 +5,7 @@ public:
         string result;
         int sizeA = a.size();
         int sizeB = b.size();
-        for (int i = sizeA - 1, j = sizeB - 1; i >= 0 || j >= 0 || sum;) {
+        for (int i = sizeA - 1, j = sizeB - 1; i >= 0 || j >= 0 || sum == 1;) {
             sum += i >= 0 ? a[i--] - '0' : 0;
             sum += j >= 0 ? b[j--] - '0' : 0;
             result.push_back(sum % 2 + '0');
--- Cpp/067_AddBinary/readme.txt
+++ Cpp/067_AddBinary/readme.txt
@@ -1,3 +1,10 @@
 001.cpp:
 Time complexity: O(n)
 Space complexity: O(1)
+My solution.
+002.cpp:
+Time complexity: O(n)
+Space complexity: O(1)
+Reference:
+https://leetcode.com/discuss/25593/short-code-by-c
+002.cpp is shorter, but I think 001.cpp is more readable
