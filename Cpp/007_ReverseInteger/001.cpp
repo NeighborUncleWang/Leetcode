bf209b49906148f6af53c0c406f56a71dba7a424
Xianshu Li <lixianshu1992@gmail.com>
2016-03-19 23:34:14 -0500

    Q007

--- Cpp/007_ReverseInteger/001.cpp
+++ Cpp/007_ReverseInteger/001.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     int reverse(int x) {
-        int num = abs(x);
-        //INT_MIN = -2147483648
-        //so reverse INT_MIN will result overflow
         int result = 0;
-        while (num) {
-            //if x == INT_MIN, abs(x) will still be negative
-            //so INT_MAX - num % 10 will be -2147483641
-            //so it will return 0 immediately
-            //don't worry about this case
-            if (result > (INT_MAX - num % 10) / 10) {
+        while (x) {
+            if (x > 0 && result > (INT_MAX - x % 10) / 10 
+            || x < 0 && result < (INT_MIN - x % 10) / 10) {
                 return 0;
             }
-            result = result * 10 + num % 10;
-            num /= 10;
+            result = result * 10 + x % 10;
+            x /= 10;
         }
-        return x < 0 ? -result : result; 
+        return result;
     }
 };
--- Cpp/007_ReverseInteger/readme.txt
+++ Cpp/007_ReverseInteger/readme.txt
@@ -1,5 +1,5 @@
 001.cpp:
 Time complexity: O(n)
 Space complexity: O(1)
-002.cpp WILL NOT WORK! 另一种试图比较result和x最高位异或值的方法也不会work，都是因为输入-10这个testcase有问题（因为result最开始那次循环会保持0，但是x还是一个负数，所以就被判成符号位不同了）
-http://blog.csdn.net/linhuanmars/article/details/20024837
+Reference:
+https://leetcode.com/discuss/18785/my-accepted-15-lines-of-code-for-java
