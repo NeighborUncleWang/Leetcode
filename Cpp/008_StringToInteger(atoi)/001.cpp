8b59bd92beb27eb78c721181fb7746d02d3c9adc
Xianshu Li <lixianshu1992@gmail.com>
2016-03-20 23:29:06 -0500

    Q008

--- Cpp/008_StringToInteger(atoi)/001.cpp
+++ Cpp/008_StringToInteger(atoi)/001.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     int myAtoi(string str) {
-        if (str.size() == 0) {
+        auto iter = str.find_first_not_of(' ');
+        if (iter == string::npos) {
             return 0;
         }
-        int i = 0;
-        bool negative = false;
+        bool isNegative = false;
+        if (str[iter] == '-' || str[iter] == '+') {
+            isNegative = str[iter++] == '-' ? true : false;
+        }
         int result = 0;
-        while (str[i] == ' ') {
-            ++i;
-        }
-        if (i == str.size()) {
-            return 0;
-        }
-        if (str[i] == '-') {
-            negative = true;
-            ++i;
-        } else if (str[i] == '+') {
-            ++i;
-        }
-        while (i < str.size() && str[i] >= '0' && str[i] <= '9') {
-            if (negative == true && result > -((INT_MIN + str[i] - '0') / 10)) {//learn how to deal with the case when result is INT_MIN
-                return INT_MIN;
-            } else if (negative == false && result > (INT_MAX - str[i] + '0') / 10) {
-                return INT_MAX;
+        while (isdigit(str[iter])) {
+            //这里一定要INT_MAX - str[iter] + '0'
+            //而不是INT_MAX + '0' - str[iter]
+            //否则INT_MAX + '0'会返回一个负数
+            //那结果就出错了
+            //但是转念一想也没事，因为一旦INT_MAX + '0'变成负数了
+            //INT_MAX + '0' - str[iter]又会变回正数
+            if (result > (INT_MAX - str[iter] + '0') / 10) {
+                return isNegative ? INT_MIN : INT_MAX;
             }
-            result = result * 10 + str[i] - '0';
-            ++i;
+            result = result * 10 + str[iter] - '0';
+            ++iter;
         }
-        return negative ? -result : result;
+        return isNegative ? -result : result;
     }
 };
--- Cpp/008_StringToInteger(atoi)/readme.txt
+++ Cpp/008_StringToInteger(atoi)/readme.txt
@@ -1,11 +1,4 @@
 001.cpp:
 Time complexity: O(n)
 Space complexity: O(1)
-002.cpp:
-Time complexity: O(n)
-Space complexity: O(1)
-003.cpp:
-Time complexity: O(n)
-Space complexity: O(1)
-003.cpp is a shorter and cleaner code.
-In 002.cpp, when result <= INT_MIN, return INT_MIN; In 001.cpp, when result < INT_MIN, return INT_MIN.
+My solution.
