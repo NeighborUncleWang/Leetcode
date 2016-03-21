cd3d1832f7396c0bc430dd85fa7204d4ce1b082d
Xianshu Li <lixianshu1992@gmail.com>
2016-03-21 15:20:07 -0500

    Q161

--- Cpp/161_OneEditDistance/001.cpp
+++ Cpp/161_OneEditDistance/001.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     bool isOneEditDistance(string s, string t) {
-        //let t be the longer string
         if (s.size() > t.size()) {
             swap(s, t);
         }
-        int m = s.size();
-        int n = t.size();
-        //n should be larger than m
-        if (n - m > 1) {
-            return false;
+        int sSize = s.size();
+        int tSize = t.size();
+        for (int i = 0; i < sSize; ++i) {
+            if (s[i] != t[i]) {
+                if (sSize == tSize) {
+                    return s.substr(i + 1) == t.substr(i + 1);
+                } else if (sSize == tSize - 1) {
+                    return s.substr(i) == t.substr(i + 1);
+                } else {
+                    return false;
+                }
+            }
         }
-        int i = 0;
-        int difference = n - m;
-        while (i < m && s[i] == t[i]) { ++i; }
-        if (i == m) {
-            return difference > 0;
-        }
-        if (difference == 0) {
-            ++i;
-        }
-        while (i < m && s[i] == t[i + difference]) { ++i; }
-        return i == m;
+        return s.size() == tSize - 1;
     }
 };
