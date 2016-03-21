6cd43d0ed65cbba343578b90d8047c439818385c
Xianshu Li <lixianshu1992@gmail.com>
2016-03-20 23:57:45 -0500

    Q008

--- Cpp/008_StringToInteger(atoi)/001.cpp
+++ Cpp/008_StringToInteger(atoi)/001.cpp
@@ -10,7 +10,7 @@ public:
             isNegative = str[iter++] == '-' ? true : false;
         }
         int result = 0;
-        while (isdigit(str[iter])) {
+        for (; iter < str.size() && isdigit(str[iter]); ++iter) {
             //这里一定要INT_MAX - str[iter] + '0'
             //而不是INT_MAX + '0' - str[iter]
             //否则INT_MAX + '0'会返回一个负数
@@ -21,7 +21,6 @@ public:
                 return isNegative ? INT_MIN : INT_MAX;
             }
             result = result * 10 + str[iter] - '0';
-            ++iter;
         }
         return isNegative ? -result : result;
     }
