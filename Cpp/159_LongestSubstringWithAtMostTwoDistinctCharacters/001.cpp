898e1b02e24c315eeb6c29818c939bace09485d1
Xianshu Li <lixianshu1992@gmail.com>
2016-03-22 00:48:02 -0500

    Q159

--- Cpp/159_LongestSubstringWithAtMostTwoDistinctCharacters/001.cpp
+++ Cpp/159_LongestSubstringWithAtMostTwoDistinctCharacters/001.cpp
@@ -6,10 +6,10 @@ public:
         int distinctNumber = 0;
         for (int start = 0, end = 0; end < s.size(); ++end) {
             //当char作为index时，因为opertator[]的input是size_type, 所以会被转化为unsigned类型
-            if (hashMap[s[end]] == 0) {
+            ++hashMap[s[end]];
+            if (hashMap[s[end]] == 1) {
                 ++distinctNumber;
             }
-            ++hashMap[s[end]];
             //其实可以把下面的loop写在if (hashMap[s[end]] == 1)里面
             //因为只有发现新的distinct number的时候才需要检查是否超过两个distinct number
             while (distinctNumber > 2) {
--- Cpp/159_LongestSubstringWithAtMostTwoDistinctCharacters/readme.txt
+++ Cpp/159_LongestSubstringWithAtMostTwoDistinctCharacters/readme.txt
@@ -8,3 +8,4 @@ https://leetcode.com/discuss/21929/share-my-c-solution
 Time complexity: O(n)
 Space complexity: O(1)
 Use unordered_map as a hash map. hashMap.size() will not exceed 256 since sizeof(char) = 1
+这种题目一般都是在for loop一开始先把新元素加到集合中，然后再进行一系列操作使得集合中元素满足题目要求，比如这题的at most k distinct characters, 然后在for loop 最后update max length。
