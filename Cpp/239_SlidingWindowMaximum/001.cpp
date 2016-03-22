f22a9ce480662762f7ec07d66242d03d4cf7bdb0
Xianshu Li <lixianshu1992@gmail.com>
2016-03-22 16:21:28 -0500

    Q239

--- Cpp/239_SlidingWindowMaximum/001.cpp
+++ Cpp/239_SlidingWindowMaximum/001.cpp
@@ -4,15 +4,17 @@ public:
         deque<int> dq;
         vector<int> result;
         for (int i = 0; i < nums.size(); ++i) {
-            //here we can either use if or while, but the while loop will only be excuted once
             if (!dq.empty() && dq.front() < i + 1 - k) {
                 dq.pop_front();
             }
             //here we should use while instead of if, otherwise can't pass [4, 3, 11], 3 case
+            //这里也可以用nums[dq.back()] <= nums[i]
             while (!dq.empty() && nums[dq.back()] < nums[i]) {
                 dq.pop_back();
             }
             dq.push_back(i);
+            //整个过程deque中的元素对应的值是单调递减的(monotonic decreasing)
+            //所以nums[dq.front()]对应的是最大值
             if (i >= k - 1) {
                 result.push_back(nums[dq.front()]);
             }
