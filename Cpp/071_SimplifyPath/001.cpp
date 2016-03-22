75a9a9e13b361d4426ab44d832d2e9f0acdd7293
Xianshu Li <lixianshu1992@gmail.com>
2016-03-21 20:00:46 -0500

    Q071

--- Cpp/071_SimplifyPath/001.cpp
+++ Cpp/071_SimplifyPath/001.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        int i = 0;
-        vector<string> st;
-        while (i < path.size()) {
-            while (i < path.size() && path[i] == '/') { ++i; }
-            if (i == path.size()) {
-                break;
-            }
-            int start = i;
-            while (i < path.size() && path[i] != '/') { ++i; }
-            int end = i;
-            string s(path.begin() + start, path.begin() + end);//will construct from [begin, end), so should be path.begin() + end instead of path.begin() + end - 1
-            //string s = path.substr(start, end - start);//another way to construct the string
-            if (s == "..") {
-                if (st.size() > 0) {
-                    st.pop_back();
-                }
-            } else if (s != ".") {
-                st.push_back(s);
+        stringstream ss(path);
+        string str;
+        vector<string> symbols;
+        while (getline(ss, str, '/')) {
+            //如果输入是"///"
+            //两个"/"分隔符之间会读入一个空的string
+            //所以要有判断str != ""
+            if (str == ".." && !symbols.empty()) {
+                symbols.pop_back();
+            } else if (str != "." && str != "" && str != "..") {
+                symbols.push_back(str);
             }
         }
         string result;
-        if (st.size() == 0) {
-            return "/";
-        } else {
-            for (int j = 0; j < st.size(); ++j) {
-                result = result + "/" + st[j];
-            }
-            return result;
+        for (string& str : symbols) {
+            result += "/" + move(str);
         }
+        return result.empty() ? "/" : result;
     }
 };
---
+++ Cpp/071_SimplifyPath/readme.txt
@@ -0,0 +1,5 @@
+001.cpp:
+Time complexity: O(n)
+Space complexity: O(n)
+Reference:
+https://leetcode.com/discuss/24939/c-10-lines-solution
