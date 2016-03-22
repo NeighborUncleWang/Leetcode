d4869a4a7900092b07dd708c377646a83f7e8646
Xianshu Li <lixianshu1992@gmail.com>
2016-03-21 21:40:52 -0500

    Q068

--- Cpp/068_TextJustification/001.cpp
+++ Cpp/068_TextJustification/001.cpp
@@ -2,49 +2,23 @@ class Solution {
 public:
     vector<string> fullJustify(vector<string>& words, int maxWidth) {
         vector<string> result;
-        string solution;
-        int count = 0;
-        int last = 0;
-        for (int i = 0; i < words.size(); ++i) {
-            if (count + words[i].size() + i - last > maxWidth) {
-                int spaceNumber = 0;
-                int extraSpace = 0;
-                // test whether i - 1 - last is 0 before use it as divisor 
-                if (i - 1 - last > 0) {
-                    spaceNumber = (maxWidth - count) / (i - 1 - last);//contain words[i - 1]
-                    extraSpace = (maxWidth - count) % (i - 1 - last);
-                }
-                for (int j = last; j < i; ++j) {
-                    solution += words[j];
-                    if (j < i - 1) {
-                        solution.append(spaceNumber, ' ');
-                        if (extraSpace) {
-                            solution.append(1, ' ');
-                            --extraSpace;
-                        }
-                    }
-                }
-                //考虑当前行只有一个单词，所以上面添加space的代码不会执行(因为i - 1 - last == 0), 所以要单独考虑，把空格一直加到行尾知道行长度达到maxWidth为止
-                for (int i = solution.size(); i < maxWidth; ++i) {
-                    solution.append(1, ' ');
-                }
-                result.push_back(solution);
-                solution.clear();
-                count = 0;
-                last = i;
+        int size = words.size();
+        for (int i = 0, k = 0, l = 0; i < size; i += k) {
+            for (l = 0, k = 0; i + k < size && l + words[i + k].size() <= maxWidth - k; ++k) {
+                l += words[i + k].size();
             }
-            count += words[i].size();
-        }
-        for (int i = last; i < words.size(); ++i) {
-            solution += words[i];
-            if (i < words.size() - 1) {
-                solution.append(1, ' ');
+            string temp = words[i];
+            for (int j = 0; j < k - 1; ++j) {
+                if (i + k >= size) {
+                    temp += ' ';
+                } else {
+                    temp.append((maxWidth - l) / (k - 1) + (j < (maxWidth - l) % (k - 1) ? 1 : 0), ' ');
+                }
+                temp += words[i + j + 1];
             }
+            temp.append(maxWidth - temp.size(), ' ');
+            result.push_back(move(temp));
         }
-        for (int i = solution.size(); i < maxWidth; ++i) {
-            solution.append(1, ' ');
-        }
-        result.push_back(solution);
         return result;
     }
 };
--- Cpp/068_TextJustification/readme.txt
+++ Cpp/068_TextJustification/readme.txt
@@ -1,3 +1,5 @@
 001.cpp:
 Time complexity: O(n)(the word in the last line will be visited twice, otherwise only once)
 Space complexity: O(maxWidth)(the size for the string solution)
+Reference:
+https://leetcode.com/discuss/13610/share-my-concise-c-solution-less-than-20-lines
