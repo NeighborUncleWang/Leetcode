e2bcefb08c31982b1c78378cd27bb0ae0b5666ba
Xianshu Li <lixianshu1992@gmail.com>
2016-03-23 20:18:13 -0500

    Q306

--- Cpp/306_AdditiveNumber/001.cpp
+++ Cpp/306_AdditiveNumber/001.cpp
@@ -3,15 +3,11 @@ public:
     bool isAdditiveNumber(string num) {
         int n = num.size();
         for (int i = 1; i <= (n - 1) / 2; ++i) {
+            if (i > 1 && num[0] == '0') continue;
             string num1 = num.substr(0, i);
-            if (num1[0] == '0' && num1.size() > 1) {
-                continue;
-            }
             for (int j = 1; n - i - j >= max(i, j); ++j) {
+                if (j > 1 && num[i] == '0') continue;
                 string num2 = num.substr(i, j);
-                if (num2[0] == '0' && num2.size() > 1) {
-                    continue;
-                }
                 if (isValid(num1, num2, i + j, num)) {
                     return true;
                 }
@@ -21,11 +17,13 @@ public:
     }
 private:
     bool isValid(string num1, string num2, int start, string& num) {
-        if (start == num.size()) {
+        string sum = addNums(num1, num2);
+        if (sum == num.substr(start)) {
             return true;
         }
-        string sum = addNums(num1, num2);
-        return num.find(sum, start) == start && isValid(num2, sum, start + sum.size(), num);
+        //don't use string::find to check that num.startsWith(sum, start)
+        //that equals to strStr problem, will increase the complexity
+        return num.substr(start, sum.size()) == sum && isValid(num2, sum, start + sum.size(), num);
     }
     string addNums(long long num1, long long num2) {
         return to_string(num1 + num2);
@@ -38,13 +36,11 @@ private:
         while (i1 >= 0 || i2 >= 0) {
             int digit = (i1 >= 0 ? num1[i1--] - '0' : 0) + (i2 >= 0 ? num2[i2--] - '0' : 0) + carry;
             carry = digit / 10;
-            digit %= 10;
-            sum.push_back(digit + '0');
+            sum.push_back(digit % 10 + '0');
         }
         if (carry) {
             sum += '1';
         }
-        reverse(sum.begin(), sum.end());
-        return sum;
+        return string(sum.rbegin(), sum.rend());
     }
 };
--- Cpp/306_AdditiveNumber/002.cpp
+++ Cpp/306_AdditiveNumber/002.cpp
@@ -3,15 +3,11 @@ public:
     bool isAdditiveNumber(string num) {
         int n = num.size();
         for (int i = 1; i <= (n - 1) / 2; ++i) {
+            if (i > 1 && num[0] == '0') continue;
             string num1 = num.substr(0, i);
-            if (num1[0] == '0' && num1.size() > 1) {
-                continue;
-            }
             for (int j = 1; n - i - j >= max(i, j); ++j) {
+                if (j > 1 && num[i] == '0') continue;
                 string num2 = num.substr(i, j);
-                if (num2[0] == '0' && num2.size() > 1) {
-                    continue;
-                }
                 if (isValid(num1, num2, num.substr(i + j))) {
                     return true;
                 }
@@ -25,8 +21,7 @@ private:
             return true;
         }
         string sum = addNums(num1, num2);
-        //rest.find(sum) == 0 checke whether rest starts with sum
-        return rest.find(sum) == 0 && isValid(num2, sum, rest.substr(sum.size()));
+        return rest.substr(0, sum.size()) == sum && isValid(num2, sum, rest.substr(sum.size()));
     }
     string addNums(long long num1, long long num2) {
         return to_string(num1 + num2);
@@ -45,7 +40,6 @@ private:
         if (carry) {
             sum += '1';
         }
-        reverse(sum.begin(), sum.end());
-        return sum;
+        return string(sum.rbegin(), sum.rend());
     }
 };
