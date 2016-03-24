475c78ade0d4ea53779249d51d172234d4feeca8
Xianshu Li <lixianshu1992@gmail.com>
2016-03-24 17:21:53 -0500

    Q126

--- Cpp/126_WordLadderII/001.cpp
+++ Cpp/126_WordLadderII/001.cpp
@@ -7,34 +7,30 @@ public:
         currentLevel.insert(start);
         unordered_map<string, vector<string>> parentMap;
         bool foundPath = false;
-        while (!currentLevel.empty() && !foundPath) {
-            for (auto &i : currentLevel) {
-                dict.erase(i);
+        while (!currentLevel.empty()) {
+            //这里必须每一个level结束时一次性清除这个level的所有node
+            for (const string& str : currentLevel) {
+                dict.erase(str);
             }
-            for (auto &word : currentLevel) {
-                for (int i = 0; i < word.size(); ++i) {
-					//这里多次调用构造函数和析构函数，可以优化一下
-                    auto newWord = word;
+            for (const string& word : currentLevel) {
+                string newWord = word;
+                for (int i = 0; i < newWord.size(); ++i) {
+                    char temp = newWord[i];
                     for (char ch = 'a'; ch <= 'z'; ++ch) {
                         newWord[i] = ch;
-                        if (newWord == word) {
-                            continue;
-                        }
                         if (dict.find(newWord) != dict.end()) {
                             if (newWord == end) {
                                 foundPath = true;
                             }
                             nextLevel.insert(newWord);
-							//这里之所以是parentMap[newWord].push_back(word)
-							//而不是partentMap[word].push_back(newWord)是为了
-							//后面构造path的时候能从确保能到达end的path从后往前往start构造
-							//如果从start往end构造的话会造成前期不知道哪条路径能到达end
-							//导致多出运行时间，这里说的都是从start往end搜的情况
-							//其实可以从end往start搜这样后面构造path时就不用reverse两次了
-							//这个后面再写了NND，今天太晚了，终于搞定了
+                            //这里之所以是parentMap[newWord].push_back(word)
+                            //而不是partentMap[word].push_back(newWord)是为了
+                            //后面构造path的时候能从确保能到达end的path从后往前往start构造
+                            //如果从start往end构造的话会造成前期不知道哪条路径能到达end
                             parentMap[newWord].push_back(word);
                         }
                     }
+                    newWord[i] = temp;
                 }
             }
             currentLevel.clear();
@@ -43,8 +39,8 @@ public:
         vector<vector<string>> result;
         vector<string> sequence;
         if (foundPath) {
-			//这里我们先push_back(end) 而不是start,因为我们是反着构造path的
-            vector<string> sequence(1, end);
+            //这里我们先push_back(end) 而不是start,因为我们是反着构造path的
+            list<string> sequence;
             getPath(end, start, parentMap, sequence, result);
             return result;
         } else {
@@ -53,17 +49,17 @@ public:
     }
 private:
     void getPath(const string& current, const string& start, unordered_map<string, vector<string>>& parentMap, 
-    vector<string>& sequence, vector<vector<string>>& result) {
+    list<string>& sequence, vector<vector<string>>& result) {
         if (current == start) {
-            reverse(sequence.begin(), sequence.end());
-            result.push_back(sequence);
-            reverse(sequence.begin(), sequence.end());
+            sequence.push_front(current);
+            result.push_back(vector<string>(sequence.begin(), sequence.end()));
+            sequence.pop_front();
             return;
         }
-        for (auto &i : parentMap[current]) {
-            sequence.push_back(i);
-            getPath(i, start, parentMap, sequence, result);
-            sequence.pop_back();
+        sequence.push_front(current);
+        for (string& word : parentMap[current]) {
+            getPath(word, start, parentMap, sequence, result);
         }
+        sequence.pop_front();
     }
 };
