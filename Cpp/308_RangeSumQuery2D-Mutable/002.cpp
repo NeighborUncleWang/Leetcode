aa1760dfd188d14853590074df00413338977ac0
Xianshu Li <lixianshu1992@gmail.com>
2016-03-25 21:38:28 -0500

    Q308

--- Cpp/308_RangeSumQuery2D-Mutable/002.cpp
+++ Cpp/308_RangeSumQuery2D-Mutable/002.cpp
@@ -1,29 +1,29 @@
 struct QuadTreeNode {
-    int sum = 0;
-    vector<QuadTreeNode*> children = vector<QuadTreeNode*>(4, nullptr);
     int row1 = 0;
     int col1 = 0;
     int row2 = 0;
     int col2 = 0;
+    int sum = 0;
+    vector<QuadTreeNode*> children = vector<QuadTreeNode*>(4, nullptr);
+    QuadTreeNode(int row1, int col1, int row2, int col2, int sum) {
+        this->row1 = row1;
+        this->row2 = row2;
+        this->col1 = col1;
+        this->col2 = col2;
+        this->sum = sum;
+    }
 };
-class QuadTree {
+class NumMatrix {
 private:
     QuadTreeNode* root;
-    vector<vector<int>> matrix;
     QuadTreeNode* BuildQuadTree(vector<vector<int>>& matrix,
     int row1, int col1, int row2, int col2) {
         if (row1 > row2 || col1 > col2) {
             return nullptr;
+        } else if (row1 == row2 && col1 == col2) {
+            return new QuadTreeNode(row1, col1, row2, col2, matrix[row1][col1]);
         }
-        auto node = new QuadTreeNode();
-        if (row1 == row2 && col1 == col2) {
-            node->sum = matrix[row1][col1];
-            node->row1 = row1;
-            node->col1 = col1;
-            node->row2 = row2;
-            node->col2 = col2;
-            return node;
-        }
+        QuadTreeNode* node = new QuadTreeNode(row1, col1, row2, col2, 0);
         int middleRow = row1 + (row2 - row1) / 2;
         int middleColumn = col1 + (col2 - col1) / 2;
         node->children[0] = BuildQuadTree(matrix, row1, col1, middleRow, middleColumn);
@@ -35,22 +35,26 @@ private:
                 node->sum += child->sum;
             }
         }
-        node->row1 = row1;
-        node->col1 = col1;
-        node->row2 = row2;
-        node->col2 = col2;
         return node;
     }
-    void updateHelper(QuadTreeNode* node, int row, int col, int diff) {
+    void updateHelper(QuadTreeNode* node, int row, int col, int val) {
         if (node == nullptr || row < node->row1 || row > node->row2
             || col < node->col1 || col > node->col2) {
             return;
+        } else if (row == node->row1 && node->row1 == node->row2
+        && col == node->col1 && node->col1 == node->col2) {
+            node->sum = val;
+            return;
         }
-        node->sum += diff;
-        updateHelper(node->children[0], row, col, diff);
-        updateHelper(node->children[1], row, col, diff);
-        updateHelper(node->children[2], row, col, diff);
-        updateHelper(node->children[3], row, col, diff);
+        for (QuadTreeNode* child : node->children) {
+            updateHelper(child, row, col, val);
+        }
+        node->sum = 0;
+        for (QuadTreeNode* child : node->children) {
+            if (child) {
+                node->sum += child->sum;
+            }
+        }
     }
     int sumRegionHelper(QuadTreeNode* node, int row1, int col1, int row2, int col2) {
         if (node == nullptr || node->row1 > row2 || node->col1 > col2
@@ -67,33 +71,18 @@ private:
         return sum;
     }
 public:
-    QuadTree(vector<vector<int>>& matrix) {
-        this->matrix = matrix;
+    NumMatrix(vector<vector<int>> &matrix) {
         int rowSize = matrix.size();
-        int columnSize = rowSize == 0 ? 0 : matrix[0].size();
+        int columnSize = rowSize ? matrix[0].size() : 0;
         root = BuildQuadTree(matrix, 0, 0, rowSize - 1, columnSize - 1);
     }
-    void updateQuadTree(int row, int col, int val) {
-        int diff = val - matrix[row][col];
-        updateHelper(root, row, col, diff);
-        matrix[row][col] = val;
-    }
-    int sumRegion(int row1, int col1, int row2, int col2) {
-        return sumRegionHelper(root, row1, col1, row2, col2);
-    }
-};
-class NumMatrix {
-private:
-    QuadTree tree;
-public:
-    NumMatrix(vector<vector<int>> &matrix) : tree(matrix) {}
 
     void update(int row, int col, int val) {
-        tree.updateQuadTree(row, col, val);
+        updateHelper(root, row, col, val);
     }
 
     int sumRegion(int row1, int col1, int row2, int col2) {
-        return tree.sumRegion(row1, col1, row2, col2);
+        return sumRegionHelper(root, row1, col1, row2, col2);
     }
 };
 
--- Cpp/308_RangeSumQuery2D-Mutable/readme.txt
+++ Cpp/308_RangeSumQuery2D-Mutable/readme.txt
@@ -10,3 +10,4 @@ Space complexity: O(mn)
 Quad Tree(it does not equals to 2-D segment tree)
 Reference:
 https://leetcode.com/discuss/71025/c-quad-tree-736ms-and-indexed-tree-492ms-based-solutions
+002.cpp速度比001.cpp慢了很多，这里用了一个技巧，update的时候先search到leaf节点，然后再回溯回来update父节点，这样可以避免适用diff变量，不需要存一个matrix的拷贝。
