03566681d2bfd46680158feb431c81e438f84b8a
Xianshu Li <lixianshu1992@gmail.com>
2016-04-02 15:24:33 -0500

    Q031

--- Cpp/031_NextPermutation/001.cpp
+++ Cpp/031_NextPermutation/001.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        int k = -1;
-        for (int i = n - 2; i >= 0; --i) {
-            if (nums[i] < nums[i + 1]) {
-                k = i;
-                break;
-            }
-        }
-        if (k == -1) {
+        int size = nums.size();
+        int i = size - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) --i;
+        if (i < 0) {
             reverse(nums.begin(), nums.end());
             return;
         }
-        int l = -1;
-        for (int i = n - 1; i >= 0; --i) {
-            if (nums[i] > nums[k]) {
-                l = i;
-                break;
-            }
-        }
-        swap(nums[k], nums[l]);
-        reverse(nums.begin() + k + 1, nums.end());
+        int j = size - 1;
+        //这里不用检查 j >= 0,因为一定能找到 nums[j] > nums[i]的情况
+        while (nums[j] <= nums[i]) --j;
+        swap(nums[i], nums[j]);
+        reverse(nums.begin() + i + 1, nums.end());
     }
 };
--- Cpp/031_NextPermutation/readme.txt
+++ Cpp/031_NextPermutation/readme.txt
@@ -2,4 +2,6 @@ Time complexity: O(n)
 Space complexity: O(1)
 Reference:
 https://leetcode.com/discuss/38247/algorithm-wikipedia-implementation-permutations-permutations
-See the detailed solution: http://fisherlei.blogspot.com/2012/12/leetcode-next-permutation.html
+See the detailed solution: 
+https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
+http://fisherlei.blogspot.com/2012/12/leetcode-next-permutation.html
