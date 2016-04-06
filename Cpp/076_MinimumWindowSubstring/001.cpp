87abbd20e97edbc5fa0dffc031509fda482308bb
Xianshu Li <lixianshu1992@gmail.com>
2016-04-06 00:54:21 -0500

    Q076

--- Cpp/076_MinimumWindowSubstring/001.cpp
+++ Cpp/076_MinimumWindowSubstring/001.cpp
@@ -7,10 +7,14 @@ public:
         }
         int left = 0;
         int right = 0;
+        //这题如果t有重复的char也要算进去，所以用t.size()
         int count = t.size();
         int minLength = INT_MAX;
         int minLeft = 0;
         while (right < s.size()) {
+            //这里s中不在t里面的char在hashMap中的值也会被减1
+            //这样才能在后面if (++map[s[left++]] == 1) ++count;中
+            //判断left扫过的char是不是在t里面
             if (--map[s[right++]] >= 0) {
                 --count;
             }
@@ -19,6 +23,10 @@ public:
                     minLength = right - left;
                     minLeft = left;
                 }
+                //这里用>=1也是可以的
+                //但是因为一旦出现++count
+                //这个while loop就跳出了
+                //所以用==也对
                 if (++map[s[left++]] == 1) ++count;
             }
         }
