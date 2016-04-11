b7ca219a5025846c55b03d9ece4937dd62045c22
Xianshu Li <lixianshu1992@gmail.com>
2016-04-10 21:09:55 -0500

    Q209

--- Cpp/209_MinimumSizeSubarraySum/001.cpp
+++ Cpp/209_MinimumSizeSubarraySum/001.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int minSubArrayLen(int s, vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0) return 0;
         int minLength = INT_MAX;
-        int left = 0;
-        int right = 0;
-        int sum = nums[left];
-        while (right < n) {
-            if (sum >= s) {
-                minLength = min(minLength, right - left + 1);
-                sum -= nums[left];
-                ++left;
-            } else {
-                ++right;
-                if (right < n) {
-                    sum += nums[right];
-                }
+        int sum = 0;
+        for (int left = 0, right = 0; right < nums.size(); ++right) {
+            sum += nums[right];
+            //假设当前left = 0, right = 2
+            //sum >= s
+            //那么所有left = 0, right > 2的组合都不可能是minLength
+            //因为长度肯定大于left = 0, right = 2情况
+            //然后left = 1, right < 2也不能是minLength
+            //因为left = 0, right < 2时sum < s
+            //所以left = 1, right < 2时一定也有sum < s
+            while (sum >= s) {
+                minLength = min(right - left + 1, minLength);
+                sum -= nums[left++];
             }
         }
         return minLength == INT_MAX ? 0 : minLength;
