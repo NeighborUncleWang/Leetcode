d1be5be10d13362d6afaed8f3a0b67cfc5a87fe4
Xianshu Li <lixianshu1992@gmail.com>
2016-04-12 01:41:30 -0500

    q017

--- Cpp/017_LetterCombinationsOfAPhoneNumber/002.cpp
+++ Cpp/017_LetterCombinationsOfAPhoneNumber/002.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        if (digits.empty()) {
-            return vector<string>();
-        }
-        vector<string> hashMap{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        if (digits.empty()) return vector<string>();
+        vector<string> mappingTable = {"", "", "abc", "def", "ghi", "jkl",
+        "mno", "pqrs", "tuv", "wxyz"};
         deque<string> result{""};
-        for (int i = 0; i < digits.size(); ++i) {
-            int currentDigit = digits[i] - '0';
+        for (char digit : digits) {
             int size = result.size();
-            for (int j = 0; j < size; ++j) {
-                for (char ch : hashMap[currentDigit]) {
+            for (int i = 0; i < size; ++i) {
+                for (char ch : mappingTable[digit - '0']) {
                     result.push_back(result.front() + ch);
                 }
                 result.pop_front();
