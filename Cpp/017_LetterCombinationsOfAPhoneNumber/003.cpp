2e21bbc73d04984533defe5435ef06cbeb2260ab
Xianshu Li <lixianshu1992@gmail.com>
2016-04-12 01:31:30 -0500

    Q017

--- Cpp/017_LetterCombinationsOfAPhoneNumber/003.cpp
+++ Cpp/017_LetterCombinationsOfAPhoneNumber/003.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return vector<string>();
         vector<string> mappingTable = {"", "", "abc", "def", "ghi", "jkl",
         "mno", "pqrs", "tuv", "wxyz"};
-        vector<string> result;
-        if (digits.size() == 0) {
-            return result;
-        }
-        result.push_back("");
-        for (string::size_type i = 0; i < digits.size(); ++i) {
-            int digit = digits[i] - '0';
+        vector<string> result{""};
+        for (char digit : digits) {
             vector<string> temp;
-            for (string::size_type j = 0; j < mappingTable[digit].size(); ++j) {
-                for (vector<string>::size_type k = 0; k < result.size(); ++k) {
-                    temp.push_back(result[k] + mappingTable[digit][j]);//don't use string::append, this will alter the element in result, use operator +, this operator can take one string and one char
+            for (char ch : mappingTable[digit - '0']) {
+                for (string& s : result) {
+                    //don't use string::append, this will alter the element in result, 
+                    //use operator +, this operator can take one string and one char
+                    temp.push_back(s + ch);
                 }
             }
             result = move(temp);
