630d326e14ce2d2d9ff34c706e854813eaadf12a
Xianshu Li <lixianshu1992@gmail.com>
2016-04-20 20:44:15 -0500

    Q068

--- Cpp/015_3Sum/001.cpp
+++ Cpp/015_3Sum/001.cpp
@@ -6,14 +6,15 @@ public:
         vector<vector<int>> result;
         for (int i = 0; i < n - 2; ++i) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
-            int left = i + 1;
-            int right = n - 1;
-            while (left < right) {
+            for (int left = i + 1, right = n - 1; left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
                 if (sum == 0) {
                     result.push_back(vector<int>{nums[i], nums[left], nums[right]});
                     ++left;
                     --right;
+                    //这里必须先++left, --right
+                    //因为如果nums里面的值都是distinct的话，后面两个while loop不会修改left和right
+                    //那样导致死循环
                     while (left < right && nums[left] == nums[left - 1]) ++left;
                     while (left < right && nums[right] == nums[right + 1]) --right;
                 } else if (sum < 0) {
--- Cpp/068_TextJustification/001.cpp
+++ Cpp/068_TextJustification/001.cpp
@@ -3,18 +3,20 @@ public:
     vector<string> fullJustify(vector<string>& words, int maxWidth) {
         vector<string> result;
         int size = words.size();
-        for (int i = 0, k = 0, l = 0; i < size; i += k) {
+        for (int i = 0, l, k; i < size; i += k) {
             for (l = 0, k = 0; i + k < size && l + words[i + k].size() <= maxWidth - k; ++k) {
                 l += words[i + k].size();
             }
             string temp = words[i];
-            for (int j = 0; j < k - 1; ++j) {
+            //这里j还是相当于index
+            //和reference里稍微有点不一样
+            for (int j = 1; j < k; ++j) {
                 if (i + k >= size) {
                     temp += ' ';
                 } else {
-                    temp.append((maxWidth - l) / (k - 1) + (j < (maxWidth - l) % (k - 1) ? 1 : 0), ' ');
+                    temp.append((maxWidth - l) / (k - 1) + (j <= (maxWidth - l) % (k - 1) ? 1 : 0), ' ');
                 }
-                temp += words[i + j + 1];
+                temp += words[i + j];
             }
             temp.append(maxWidth - temp.size(), ' ');
             result.push_back(move(temp));
