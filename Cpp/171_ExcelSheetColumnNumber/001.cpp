60a8dc5bdadf20377aba39c8c9787d4211a618b6
Xianshu Li <lixianshu1992@gmail.com>
2016-04-24 15:00:47 -0500

    Q171

--- Cpp/171_ExcelSheetColumnNumber/001.cpp
+++ Cpp/171_ExcelSheetColumnNumber/001.cpp
@@ -3,8 +3,7 @@ public:
     int titleToNumber(string s) {
         int result = 0;
         for (int i = 0; i < s.size(); ++i) {
-            result *= 26;
-            result += s[i] - 'A' + 1;
+            result = result * 26 + s[i] - 'A' + 1;
         }
         return result;
     }
