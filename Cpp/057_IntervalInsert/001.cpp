3041562a4235e31401256765fab9f69a8e8525a9
Xianshu Li <lixianshu1992@gmail.com>
2016-04-23 21:13:38 -0500

    Q125

--- Cpp/057_IntervalInsert/001.cpp
+++ Cpp/057_IntervalInsert/001.cpp
@@ -16,6 +16,10 @@ public:
             result.push_back(*iter++);
         }
         while (iter != intervals.end() && iter->start <= newInterval.end) {
+            //其实newInterval.start = min(iter->start, newInterval.start);只需要对这个while loop
+            //里的第一个iter做，后面的其实不需要
+            //后面的iter指向的interval只会更新newInterval.end
+            //但是为了满足条件的iter单独写一个if statement代码会比较难看，所以就不那样写了
             newInterval.start = min(iter->start, newInterval.start);
             newInterval.end = max(iter->end, newInterval.end);
             ++iter;
--- Cpp/125_ValidPalindrome/001.cpp
+++ Cpp/125_ValidPalindrome/001.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
+        //也可以直接写transform(s.begin(), s.end(), s.begin(), ::tolower);
+        //tolower前面一定要加::,否则编译通不过，不知道为什么
         transform(s.begin(), s.end(), s.begin(), [](char ch) { return tolower(ch); });
         int n = s.size();
         int left = 0;
