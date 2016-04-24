5885181e08e09e1b797c82f1134607e4435e19b4
Xianshu Li <lixianshu1992@gmail.com>
2016-04-23 21:13:25 -0500

    Q079

--- Cpp/079_WordSearch/001.cpp
+++ Cpp/079_WordSearch/001.cpp
@@ -6,29 +6,32 @@ public:
         vector<vector<bool>> visited(rowSize, vector<bool>(columnSize, false));
         for (int i = 0; i < rowSize; ++i) {
             for (int j = 0; j < columnSize; ++j) {
-                if (dfs(board, word, rowSize, columnSize, visited, i, j, 0))
+                if (dfs(board, word, visited, i, j, 0))
                     return true;
             }
         }
         return false;
     }
-    bool dfs(vector<vector<char>>& board, string& word, int rowSize, int columnSize,
+    bool dfs(vector<vector<char>>& board, string& word,
     vector<vector<bool>>& visited, int i, int j, int index) {
         if (index == word.size()) {
             return true;
         }
-        if (i < 0 || i >= rowSize || j < 0 || j >= columnSize
-        || board[i][j] != word[index] || visited[i][j] == true) {
+        //这题必须要在递归刚进来的时候再检查i,j是否在边界内
+        //否则["A"], "A"这种case过不了
+        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size()
+        || board[i][j] != word[index] || visited[i][j]) {
             return false;
         }
         visited[i][j] = true;
         bool result;
-        if (dfs(board, word, rowSize, columnSize, visited, i, j - 1, index + 1)
-        || dfs(board, word, rowSize, columnSize, visited, i, j + 1, index + 1)
-        || dfs(board, word, rowSize, columnSize, visited, i - 1, j, index + 1)
-        || dfs(board, word, rowSize, columnSize, visited, i + 1, j, index + 1)) {
-            //如果返回值为true，就不用把visited[i][j]复原了
-            //因为函数到这里开始回溯就结束了
+        //不过i+1,i-1,j+1,j-1是否在边界内，都要调用dfs函数
+        //这样才能让index+1顺利到达word.size()
+        //这题和其他在矩阵里找路径的题目的解法不同
+        if (dfs(board, word, visited, i, j - 1, index + 1)
+        || dfs(board, word, visited, i, j + 1, index + 1)
+        || dfs(board, word, visited, i - 1, j, index + 1)
+        || dfs(board, word, visited, i + 1, j, index + 1)) {
             return true;
         } else {
             visited[i][j] = false;
