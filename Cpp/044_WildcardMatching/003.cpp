58fdef9623eb491f46e9b5595382073f215048c6
Xianshu Li <lixianshu1992@gmail.com>
2016-04-24 19:22:32 -0500

    Q044

--- Cpp/044_WildcardMatching/003.cpp
+++ Cpp/044_WildcardMatching/003.cpp
@@ -8,18 +8,19 @@ public:
         while (sIndex < s.size()) {
             //之所以先判断p[pIndex] == '*'而不是s[sIndex] == p[pIndex]是为了防止s[sIndex] == '*'
             //导致错过了对p中'*'的标记
+            //不能一开始在while loop中判断if (p == p.size()) break;
+            //或者if (p == p.size()) return false;
+            //那样都会出错，只能每次access p的时候检查pIndex < p.size()
             if (pIndex < p.size() && p[pIndex] == '*') {
                 sMatch = sIndex;
                 //也可以写成starIndex = pIndex++;
-                starIndex = pIndex;
-                ++pIndex;
+                starIndex = pIndex++;
             } else if (pIndex < p.size() && (p[pIndex] == s[sIndex] || p[pIndex] == '?')) {
                 ++sIndex;
                 ++pIndex;
             } else if (starIndex != -1) {
                 pIndex = starIndex + 1;
-                ++sMatch;
-                sIndex = sMatch;
+                sIndex = ++sMatch;
             } else {
                 return false;
             }
--- Cpp/044_WildcardMatching/readme.txt
+++ Cpp/044_WildcardMatching/readme.txt
@@ -1,14 +1,21 @@
 001.cpp:
 Time complexity: O(m * n)
 Space complexity: O(m * n)
+2D DP solution.
 This is a good explanation to this solution:
 http://www.1point3acres.com/bbs/thread-118735-1-1.html
 002.cpp:
 Time complexity: O(m * n)
 Space complexity: O(n)(n is the size of string p)
+1D DP solution.
 003.cpp:
 Time complexity: O(m * n)(the author claims this a linear solution, why?)
+I think the time complexity is O(p * (s - p))
+Consider this case:
+s = "aaaaaaaaaaaaaaaaaaaab"
+p = "*aaaaaaaaaaaaaaaaaab"
 Space complexity: O(1)
+A greedy solution.
 Reference:
 https://leetcode.com/discuss/10133/linear-runtime-and-constant-space-solution
 004.cpp:
