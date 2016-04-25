d6b090be7caa0a8ac974566b5ab3368bf9ec0473
Xianshu Li <lixianshu1992@gmail.com>
2016-04-24 19:54:49 -0500

    Q274

--- Cpp/274_H-Index/001.cpp
+++ Cpp/274_H-Index/001.cpp
@@ -12,6 +12,13 @@ public:
             //A scientist has index h if h of his/her N papers have at least h citations each...
             //"h of his/her N papers"，表明一定有一个index和h相等
             //"have at least h citations each"，表明不一定有一篇paper的citation数和H-index相等
+            //另一种记法，记住editorial solution里说的H-index是把citation按照
+            //desending order排列起来与y=x围成的矩形面积
+            //因为paper数目是以1为最小单位变化的，但是citation不是
+            //所以肯定H-index肯定是和某个paper的编号一样
+            //但是不一定和某个citation值一样
+            //但是用counting sort的方法counting的array里citation也是
+            //按照1为最小单位变化，所以最后可以返回一个citation的值
             if (i + 1 > citations[i]) return i;
         }
         return citations.size();
