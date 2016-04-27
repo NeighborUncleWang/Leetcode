086053127dcbd0b0f25da1da8d6d164dad24518e
Xianshu Li <lixianshu1992@gmail.com>
2016-04-26 19:41:54 -0500

    Q324

--- Cpp/324_WiggleSortII/002.cpp
+++ Cpp/324_WiggleSortII/002.cpp
@@ -7,6 +7,9 @@ public:
         //要保证small numbers >= large numbers
         //因为数组下标从0开始，所以number of even indices >= number of odd indices
         //然后保证nums.size()为奇数或者偶数时small一直不少于large就行
+        //这题不能把nums排成ascending order然后small = 0, large = (nums.size() + 1) / 2
+        //那样会存在两个medium相邻的情况
+        //比如input[4,5,5,6]会输出[4,5,5,6]
         int large = 0;
         int small = nums.size() / 2;
         for (int i = 0; i < nums.size(); ++i) {
