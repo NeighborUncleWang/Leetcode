800aafcceeb2d82b00de3bb103e98e18de8acd30
Xianshu Li <lixianshu1992@gmail.com>
2016-05-12 16:40:25 -0500

    Q167

--- Cpp/167_TwoSumII-InputArrayIsSorted/001.cpp
+++ Cpp/167_TwoSumII-InputArrayIsSorted/001.cpp
@@ -8,6 +8,9 @@ public:
             if (sum == target) {
                 return vector<int>{left + 1, right + 1};
             } else if (sum < target) {
+                //因为[left, right - 1], [left, right - 2]这些pair的sum都会小于target
+                //不符合要求，所以要++left寻找新的pair
+                //下面的--right同理
                 ++left;
             } else if (sum > target) {
                 --right;
