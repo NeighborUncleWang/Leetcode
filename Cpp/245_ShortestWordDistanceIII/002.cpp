da33970cf0bde8f541613289287f148d7234df79
Xianshu Li <lixianshu1992@gmail.com>
2016-05-22 16:10:20 -0500

    Q245

--- Cpp/245_ShortestWordDistanceIII/002.cpp
+++ Cpp/245_ShortestWordDistanceIII/002.cpp
@@ -7,6 +7,9 @@ public:
         for (int i = 0; i < words.size(); ++i) {
             if (words[i] == word1) {
                 index1 = i;
+                //这里写成if (index2 != -1)就能通过OJ了
+                //因为index1一直是先于index2被更新的
+                //所以只要words[i] == word2时检查index2 != index1就行了
                 if (index2 != -1 && index2 != index1) {
                     minDistance = min(minDistance, abs(index2 - index1));
                 }
