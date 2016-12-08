ca878eac4b1cae5f51dc8b02a930f8fb675c91e5
Xianshu Li <lixianshu1992@gmail.com>
2016-12-08 15:35:29 -0600

    Q397

---
+++ Cpp/397_IntegerReplacement/001.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int integerReplacement(int n) {
+        int count = 0;
+        while (n != 1) {
+            if (n % 2 == 0) {
+                //必须要加&0x7FFFFFFF,因为c++ >>有可能是arithmetic shift
+                //也可能是logic shift,与平台有关
+                n = (n >> 1) & 0x7FFFFFFF;
+            //(n >> 1) & 1外面一定要加括号，因为&优先级比==低
+            } else if (n == 3 || ((n >> 1) & 1) == 0) {
+                --n;
+            } else {
+                ++n;
+            }
+            ++count;
+        }
+        return count;
+    }
+};
---
+++ Cpp/397_IntegerReplacement/readme.txt
@@ -0,0 +1,7 @@
+001.cpp:
+Time complexity: O(n)
+Space complexity: O(n)
+Recursive version.
+Reference:
+https://discuss.leetcode.com/topic/58454/0-ms-c-recursion-solution-with-explanation
+make the number has most trailing zeroes
