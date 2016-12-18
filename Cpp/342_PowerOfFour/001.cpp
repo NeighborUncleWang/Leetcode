aa6eb3be54d6595de685197f2c0e9a3170e14cab
Xianshu Li <lixianshu1992@gmail.com>
2016-12-18 17:27:36 -0600

    Q342

--- Cpp/342_PowerOfFour/001.cpp
+++ Cpp/342_PowerOfFour/001.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isPowerOfFour(int num) {
+    	//把(num & (num - 1)) == 0换成(num & -num) == num也可以
+    	//num & -num得到最低位的1
         return num > 0 && (num & (num - 1)) == 0 && (num & 0x55555555) == num;
     }
 };
