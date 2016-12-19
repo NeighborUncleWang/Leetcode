d52954360e0fa3333be09a353d0abb68262717b7
Xianshu Li <lixianshu1992@gmail.com>
2016-12-19 00:57:52 -0600

    Q421

---
+++ Cpp/421_MaximumXOROfTwoNumbersInAnArray/001.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMaximumXOR(vector<int>& nums) {
+        unordered_set<int> prefix;
+        int mask = 0;
+        int result = 0;
+        for (int i = 31; i >= 0; --i) {
+            mask |= (1 << i);
+            prefix.clear();
+            for (int num : nums) {
+                prefix.insert(num & mask);
+            }
+            int temp = result | (1 << i);
+            for (int pre : prefix) {
+                if (prefix.find(pre ^ temp) != prefix.end()) {
+                    result = temp;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+};
---
+++ Cpp/421_MaximumXOROfTwoNumbersInAnArray/readme.txt
@@ -0,0 +1,6 @@
+001.cpp:
+Time complexity: O(n)
+Space complexity: O(1)
+主要用到 if A^B=C then A^C=B这个性质
+Reference:
+https://discuss.leetcode.com/topic/63213/java-o-n-solution-using-bit-manipulation-and-hashmap/17
