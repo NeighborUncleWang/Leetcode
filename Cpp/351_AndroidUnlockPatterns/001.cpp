fe99357bd35eac0e1d4ea3f2c4cbaac8bbf6e97d
Xianshu Li <lixianshu1992@gmail.com>
2016-12-27 19:07:52 -0600

    Q351

---
+++ Cpp/351_AndroidUnlockPatterns/001.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int numberOfPatterns(int m, int n) {
+        vector<vector<int>> skip(10, vector<int>(10, 0));
+        skip[1][3] = skip[3][1] = 2;
+        skip[7][9] = skip[9][7] = 8;
+        skip[1][7] = skip[7][1] = 4;
+        skip[3][9] = skip[9][3] = 6;
+        skip[1][9] = skip[9][1] = skip[2][8] = skip[8][2] = skip[3][7] = skip[7][3] = skip[4][6] = skip[6][4] = 5;
+        int result = 0;
+        vector<bool> visited(10, false);
+        for (int i = m; i <= n; ++i) {
+            result += dfs(skip, visited, 1, i - 1) * 4;
+            result += dfs(skip, visited, 2, i - 1) * 4;
+            result += dfs(skip, visited, 5, i - 1);
+        }
+        return result;
+    }
+private:
+    int dfs(vector<vector<int>>& skip, vector<bool>& visited, int current, int remain) {
+        if (remain == 0) return 1;
+        visited[current] = true;
+        int result = 0;
+        for (int i = 1; i <= 9; ++i) {
+            if (!visited[i] && (!skip[current][i] || visited[skip[current][i]])) {
+                result += dfs(skip, visited, i, remain - 1);
+            }
+        }
+        visited[current] = false;
+        return result;
+    }
+};
---
+++ Cpp/351_AndroidUnlockPatterns/readme.txt
@@ -0,0 +1,6 @@
+001.cpp:
+Time complexity: O(3 * sigma(i = m to n)(8! / (9 - i)!)
+Complexity: https://discuss.leetcode.com/topic/46260/java-dfs-solution-with-clear-explanations-and-optimization-beats-97-61-12ms/13
+Space complexity: O(9)
+Reference:
+https://discuss.leetcode.com/topic/46260/java-dfs-solution-with-clear-explanations-and-optimization-beats-97-61-12ms
