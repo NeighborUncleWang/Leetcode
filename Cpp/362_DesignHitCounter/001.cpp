235c16f2cdaac4d782532de82281b8e63339ef16
Xianshu Li <lixianshu1992@gmail.com>
2017-01-06 23:50:27 -0600

    Q362

---
+++ Cpp/362_DesignHitCounter/001.cpp
@@ -0,0 +1,41 @@
+class HitCounter {
+private:
+    vector<int> hits = vector<int>(300, 0);
+    vector<int> times = vector<int>(300, 0);
+public:
+    /** Initialize your data structure here. */
+    HitCounter() {
+        
+    }
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    void hit(int timestamp) {
+        int index = timestamp % 300;
+        if (times[index] == timestamp) {
+            ++hits[index];
+        } else {
+            times[index] = timestamp;
+            hits[index] = 1;
+        }
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    int getHits(int timestamp) {
+        int result = 0;
+        for (int i = 0; i < 300; ++i) {
+            if (timestamp - times[i] < 300) {
+                result += hits[i];
+            }
+        }
+        return result;
+    }
+};
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */
---
+++ Cpp/362_DesignHitCounter/readme.txt
@@ -0,0 +1,15 @@
+001.cpp:
+Time complexity: 
+hit:O(1)
+getHits:O(1)
+Space complexity: O(1)
+Reference:
+https://discuss.leetcode.com/topic/48758/super-easy-design-o-1-hit-o-s-gethits-no-fancy-data-structure-is-needed
+002.cpp:
+Time complexity: 
+hit:O(1)
+getHits:O(n)
+Space complexity: O(n)
+This solution is much shorter, but the complexity is not as good as 001.cpp.
+Reference:
+https://discuss.leetcode.com/topic/48752/simple-java-solution-with-explanation
