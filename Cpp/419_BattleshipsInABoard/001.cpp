e3dcf75985f5e1f2bc29af4d28d1a4eb6ba82209
Xianshu Li <lixianshu1992@gmail.com>
2017-01-08 21:52:54 -0600

    Q419

---
+++ Cpp/419_BattleshipsInABoard/001.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        int row = board.size();
+        int column = row ? board[0].size() : 0;
+        int count = 0;
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < column; ++j) {
+                if (board[i][j] == 'X' && (i == 0 || i > 0 && board[i - 1][j] != 'X') && 
+                (j == 0 || j > 0 && board[i][j - 1] != 'X')) {
+                    ++count;
+                }
+            }
+        }
+        return count;
+    }
+};
---
+++ Cpp/419_BattleshipsInABoard/readme.txt
@@ -0,0 +1,5 @@
+001.cpp:
+Time complexity: O(mn)
+Space complexity: O(1)
+Reference:
+https://discuss.leetcode.com/topic/62970/simple-java-solution
