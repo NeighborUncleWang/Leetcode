f2a190b54079965f550a6bd24278ff7779d79902
Xianshu Li <lixianshu1992@gmail.com>
2017-01-19 13:00:50 -0600

    Q485

---
+++ Cpp/485_MaxConsecutiveOnes/001.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count = 0, result = 0;
+        for (int i = 0; i < (int)nums.size() - 1; ++i) {
+            if (nums[i] != nums[i + 1]) {
+                
+            }
+        }
+        return result;
+    }
+};
---
+++ Cpp/485_MaxConsecutiveOnes/readme.txt
@@ -0,0 +1,5 @@
+001.cpp:
+Time complexity: O(n)
+Space complexity: O(1)
+Reference:
+https://discuss.leetcode.com/topic/75430/easy-java-solution
