a0e069d8b18a854b9521ea644f2648ca5c738058
Xianshu Li <lixianshu1992@gmail.com>
2017-02-12 20:05:19 -0600

    084

--- Cpp/084_LargestRectangleInHistogram/001.cpp
+++ Cpp/084_LargestRectangleInHistogram/001.cpp
@@ -5,10 +5,10 @@ public:
         stack<int> indices;
         int i = 0;
         int maxArea = 0;
-        while(i < height.size()){
-            if(indices.empty() || height[indices.top()] <= height[i]){
+        while (i < height.size()) {
+            if (indices.empty() || height[indices.top()] <= height[i]) {
                 indices.push(i++);
-            }else {
+            } else {
                 int t = indices.top();
                 indices.pop();
                 maxArea = max(maxArea, height[t] * (indices.empty() ? i : i - indices.top() - 1));
