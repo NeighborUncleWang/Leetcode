7aedf800738bf2c6cfe5a423b520034ff23b6f0e
Xianshu Li <lixianshu1992@gmail.com>
2017-02-16 14:27:14 -0600

    Q418

--- Cpp/418_SentenceScreenFitting/001.cpp
+++ Cpp/418_SentenceScreenFitting/001.cpp
@@ -8,6 +8,10 @@ public:
         //不需要s.pop_back()
         int size = s.size();
         vector<int> map(size, 0);
+        //map记录的是需要对start位置做出的修正值
+        //如果当前位置是空格说明previous row最后一个word没有加空格
+        //因为每个row不可能以空格开头，所以可以跳过
+        //如果当前位置不是空格，则记录需要减去多少值才能达到上一个空格的位置
         for (int i = 1; i < size; ++i) {
             map[i] = s[i] == ' ' ? 1 : map[i - 1] - 1;
         }
--- Cpp/418_SentenceScreenFitting/readme.txt
+++ Cpp/418_SentenceScreenFitting/readme.txt
@@ -1,5 +1,6 @@
 001.cpp:
 Time complexity: O(n)
 Space complexity: O(n)(n is the total size of the sentence)
+可以先看看这个post的最开始的解法:https://discuss.leetcode.com/topic/62455/21ms-18-lines-java-solution,这个比较好理解，再看下面的优化过的dp解法。
 Reference:
 https://discuss.leetcode.com/topic/62455/21ms-18-lines-java-solution/6
