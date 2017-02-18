a1685bd2a4bbc61dd97f731a1559bde41b9335bf
Xianshu Li <lixianshu1992@gmail.com>
2017-02-17 22:36:09 -0600

    Q163

--- Cpp/163_MissingRanges/001.cpp
+++ Cpp/163_MissingRanges/001.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
         //next is the next number to find
-        int next = lower;
+        long long next = lower;
         vector<string> result;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] == next) {
+        for (int num : nums) {
+            if (num == next) {
                 ++next;
-            } else if (nums[i] > next) {
-                result.push_back(getRange(next, nums[i] - 1));
-                next = nums[i] + 1;
+            } else if (num > next) {
+                result.push_back(getRange(next, num - 1));
+                //强制类型转化为long long防止num此时为INT_MAX
+                //有点hack的味道
+                next = (long long)num + 1;
             }
         }
         if (upper >= next) result.push_back(getRange(next, upper));
