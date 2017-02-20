dcf69803bf7694add26026e066b9b98c37d2fa02
Xianshu Li <lixianshu1992@gmail.com>
2017-02-19 18:06:51 -0600

    Q391

--- Cpp/391_PerfectRectangle/001.cpp
+++ Cpp/391_PerfectRectangle/001.cpp
@@ -23,6 +23,7 @@ public:
         //不能只检查points.size() == 4
         //还要检查留下来的4个点是不是矩形最外面的4个点
         //否则[[0,0,1,1],[0,0,2,1],[1,0,2,1],[0,2,2,3]]case 过不去
+        //最后要检查的是剩下的四个点是最边沿的四个点并且所围成的面积是之前所有矩形面积之和
         if (!points.count(to_string(x1) + " " + to_string(y1)) || !points.count(to_string(x2) + " " + to_string(y1)) ||
         !points.count(to_string(x1) + " " + to_string(y2)) || !points.count(to_string(x2) + " " + to_string(y2))) {
             return false;
