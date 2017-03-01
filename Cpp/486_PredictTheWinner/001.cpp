7574d6f978c1c63dbfa90e376b4b9512462cdb13
Xianshu Li <lixianshu1992@gmail.com>
2017-02-28 21:52:30 -0600

    Q486

--- Cpp/486_PredictTheWinner/001.cpp
+++ Cpp/486_PredictTheWinner/001.cpp
@@ -7,6 +7,8 @@ public:
     }
 private:
     int helper(vector<int>& nums, int start, int end, vector<vector<int*>>& mem) {
+        //这里不能通过初始化mem[start][end]为负的方式来判断
+        //因为有可能通过计算之后mem[start][end]最终为负值
         if (mem[start][end] == nullptr) {
             mem[start][end] = new int(start == end ? nums[start] : max(nums[start] - helper(nums, start + 1, end, mem),
             nums[end] - helper(nums, start, end - 1, mem)));
