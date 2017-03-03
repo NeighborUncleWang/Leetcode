d8b14281210e2f1bba9d0b458cc2c02816cbc45b
Xianshu Li <lixianshu1992@gmail.com>
2017-03-02 22:37:45 -0600

    Q022

--- Cpp/022_GenerateParentheses/001.cpp
+++ Cpp/022_GenerateParentheses/001.cpp
@@ -6,23 +6,23 @@ public:
             return result;
         }
         string sequence;
-        helper(sequence, result, 0, 0, 0, n);
+        helper(sequence, result, 0, 0, n);
         return result;
     }
 private:
-    void helper(string& sequence, vector<string>& result, int leftNumber, int rightNumber, int deep, int n) {
-        if (deep == 2 * n) {//actually we can test(rightNum == n && leftNum == n) here
+    void helper(string& sequence, vector<string>& result, int leftNumber, int rightNumber, int n) {
+        if (leftNumber == n && rightNumber == n) {//actually we can test(rightNum == n && leftNum == n) here
             result.push_back(sequence);
             return;
         }
         if (leftNumber < n) {//actually the if (leftNumber < n) and if (rightNumber < leftNumber) can swap their order(first test rightNumber < leftNumber)
             sequence.push_back('(');
-            helper(sequence, result, leftNumber + 1, rightNumber, deep + 1, n);
+            helper(sequence, result, leftNumber + 1, rightNumber, n);
             sequence.pop_back();
         }
         if (rightNumber < leftNumber) {
             sequence.push_back(')');
-            helper(sequence, result, leftNumber, rightNumber + 1, deep + 1, n);
+            helper(sequence, result, leftNumber, rightNumber + 1, n);
             sequence.pop_back();
         }
     }
