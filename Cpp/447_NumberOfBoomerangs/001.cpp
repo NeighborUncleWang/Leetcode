c87bebe929fa7fa09753e73cc16ffbecba5fc3d6
Xianshu Li <lixianshu1992@gmail.com>
2017-03-02 21:14:53 -0600

    Q447

--- Cpp/447_NumberOfBoomerangs/001.cpp
+++ Cpp/447_NumberOfBoomerangs/001.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     int numberOfBoomerangs(vector<pair<int, int>>& points) {
-        int booms = 0;
-        for (auto p : points) {
-            //gives the map an initial size can make the program faster
-            unordered_map<double, int> map(points.size());
-            for (auto q : points) {
-                booms += 2 * map[hypot(p.first - q.first, p.second - q.second)]++;
+        int res = 0;
+        unordered_map<long long, int> distances;
+        auto f = [](pair<int, int> lhs, pair<int, int> rhs) {
+            return (long long)pow(lhs.first - rhs.first, 2) + (long long)pow(lhs.second - rhs.second, 2);
+        };
+        for (int i = 0; i < points.size(); ++i) {
+            for (int j = 0; j < points.size(); ++j) {
+                if (i == j) continue;
+                distances[f(points[i], points[j])]++;
             }
+            for (auto& it : distances) {
+                res += it.second * (it.second - 1);
+            }
+            distances.clear();
         }
-        return booms;
+        return res;
     }
 };
--- Cpp/447_NumberOfBoomerangs/readme.txt
+++ Cpp/447_NumberOfBoomerangs/readme.txt
@@ -1,5 +1,5 @@
 001.cpp
-Time complexity: O(n)
-Space complexity: O(1)
+Time complexity: O(n^2)
+Space complexity: O(n)
 Reference:
-https://discuss.leetcode.com/topic/66574/7-lines-1050-ms-c/2
+https://discuss.leetcode.com/topic/66587/clean-java-solution-o-n-2-166ms
