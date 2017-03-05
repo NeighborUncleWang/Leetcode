3ec26a037597457a2fb355add56eed3c329d0e30
Xianshu Li <lixianshu1992@gmail.com>
2017-03-04 20:17:15 -0600

    Q240

--- Cpp/240_SearchA2DMatrixII/001.cpp
+++ Cpp/240_SearchA2DMatrixII/001.cpp
@@ -5,6 +5,8 @@ public:
         if (rowSize == 0) {
             return false;
         }
+        //起始位置必须选[0, col - 1]或者[row - 1, 0]
+        //每次移动都能够排除当前行或者列剩下没检查的过的元素
         for (int i = rowSize - 1, j = 0; i >= 0 && j < matrix[0].size(); ) {
             if (matrix[i][j] == target) {
                 return true;
