4e5f907271b054e4c4183ca9800d31cc76f4d14c
Xianshu Li <lixianshu1992@gmail.com>
2017-03-04 21:54:17 -0600

    Q272

--- Cpp/272_ClosestBinarySearchTreeValueII/001.cpp
+++ Cpp/272_ClosestBinarySearchTreeValueII/001.cpp
@@ -41,6 +41,11 @@ public:
                 result.push_back(current->val);
                 current = current->right;
                 //这里存的是下一个closest successor,值比current->val更大，也就是更远离target
+                //之所以要把沿途的node也存进stack是根据CLRS上找predecessor的函数
+                //"if the right subtree of node x is empty and x has a successor y, 
+                //then y is the lowest ancestor of x whose left child is also an ancestor of x"
+                //所以successor会在ancestor里，那样的话需要parent指针来寻找successor
+                //在不修改TreeNode添加parent pointer的情况下，最好的找successor方法是存下acestor到stack中
                 while (current) {
                     stackSuccessor.push(current);
                     current = current->left;
