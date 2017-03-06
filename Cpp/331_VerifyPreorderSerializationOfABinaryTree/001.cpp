c5f5d9bdf02da1705184e04bf25413fc110d19fa
Xianshu Li <lixianshu1992@gmail.com>
2017-03-05 19:19:14 -0600

    Q331

--- Cpp/331_VerifyPreorderSerializationOfABinaryTree/001.cpp
+++ Cpp/331_VerifyPreorderSerializationOfABinaryTree/001.cpp
@@ -9,6 +9,10 @@ public:
         }
         int position = 1;
         for (string& node : nodes) {
+            //这里之所以要检查position == 0是因为
+            //当node != "#"的时候，虽然是++position
+            //但是其实是--position;position += 2;两条语句顺序执行的效果
+            //所以进loop的时候要检查position == 0
             if (position == 0) return false;
             if (node == "#") {
                 --position;
