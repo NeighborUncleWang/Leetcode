f20e73afc316e4688f7da052c2e4b82cd6d83731
Xianshu Li <lixianshu1992@gmail.com>
2017-03-13 01:28:27 -0500

    Q330

--- Cpp/330_PatchingArray/001.cpp
+++ Cpp/330_PatchingArray/001.cpp
@@ -4,6 +4,7 @@ public:
         long long miss = 1;
         int added = 0;
         int i = 0;
+        //这里while条件必须是miss <= n而不是 i < nums.size()
         while (miss <= n) {
             if (i < nums.size() && nums[i] <= miss) {
                 miss += nums[i++];
