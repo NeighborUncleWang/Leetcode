219d706cdbc86cae1445279930d5f8d3d9f54d79
Xianshu Li <lixianshu1992@gmail.com>
2017-03-12 21:11:47 -0500

    Q354

--- Cpp/354_RussianDollEnvelopes/001.cpp
+++ Cpp/354_RussianDollEnvelopes/001.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int maxEnvelopes(vector<pair<int, int>>& envelopes) {
+        //[3, 4] cannot contains [3, 3], 
+        //so we need to put [3, 4] before [3, 3] 
+        //when sorting otherwise it will be counted as an increasing number 
+        //if the order is [3, 3], [3, 4]
         auto comp = [](pair<int, int> left, pair<int, int> right) {
             if (left.first < right.first) {
                 return true;
