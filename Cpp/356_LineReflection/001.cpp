6415c8608c662fdb9063c212aa50a4e672753d23
Xianshu Li <lixianshu1992@gmail.com>
2017-03-16 00:17:00 -0500

    Q356

--- Cpp/356_LineReflection/001.cpp
+++ Cpp/356_LineReflection/001.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     bool isReflected(vector<pair<int, int>>& points) {
+        //这题重复的点只计算一次，比如[[-16,1],[16,1],[16,1]]
+        //要返回true
+        //如果要计算多次的话map就要改成unordered_map<int, unordered_map<int, int>>
+        //把每个值出现的次数也计算进去
         unordered_map<int, unordered_set<int>> map;
         int min_x = INT_MAX, max_x = INT_MIN;
         for (auto point : points) {
@@ -12,7 +16,7 @@ public:
         for (auto& it : map) {
             for (int x : it.second) {
                 if (2 * x != sum) {
-                    if (it.second.count(sum - x) == 0) {
+                    if (it.second.find(sum - x) == it.second.end()) {
                         return false;
                     }
                 }
