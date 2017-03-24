3b84c1e60ede15991c1bf48b952080e41ea6efcf
Xianshu Li <lixianshu1992@gmail.com>
2017-03-23 23:20:34 -0500

    Q316

--- Cpp/316_RemoveDuplicateLetters/001.cpp
+++ Cpp/316_RemoveDuplicateLetters/001.cpp
@@ -10,6 +10,7 @@ public:
         //counts[ch - 'a']表示前面还剩下多少个ch
         //实际上在这个for loop中result里面s中的每个char最多只出现一次
         for (char ch : s) {
+            //--count[ch - 'a']要写在if statement的外面
             --counts[ch - 'a'];
             if (!inResult[ch - 'a']) {
                 while (!result.empty() && result.back() > ch && counts[result.back() - 'a']) {
