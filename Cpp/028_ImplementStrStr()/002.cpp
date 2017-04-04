5ab9e84e5d4d561460829b59aefd2ec623b5b2e3
Xianshu Li <lixianshu@utexas.edu>
2017-04-04 02:55:22 -0500

    Q028

--- Cpp/028_ImplementStrStr()/002.cpp
+++ Cpp/028_ImplementStrStr()/002.cpp
@@ -3,8 +3,9 @@ public:
     int strStr(string haystack, string needle) {
         int size1 = haystack.size();
         int size2 = needle.size();
-        if (size2 == 0) return 0;
-        if (size2 > size1) return -1;
+        //不用下面两个statement程序也是对的，也能pass oj
+        // if (size2 == 0) return 0;
+        // if (size2 > size1) return -1;
         for (int i = 0; i <= size1 - size2; ++i) {
             int j = 0;
             for (; j < size2 && needle[j] == haystack[i + j]; ++j) {}
--- Cpp/439_TernaryExperssionParser/readme.txt
+++ Cpp/439_TernaryExperssionParser/readme.txt
@@ -1,6 +1,7 @@
 001.cpp:
 Time complexity: O(n)
 Space compelxity: O(n)
+从后往前找到第一个'?'来判断一个三元表达式的值，找':'没有用。因为三元表达式group right-to-left（右结合），所以要从右往左找，然后因为要判断一个三元表达式必须要判断'?'之前的condition是true or false,所以要先找到'?'而不是':'
 Reference:
 https://discuss.leetcode.com/topic/64409/very-easy-1-pass-stack-solution-in-java-no-string-concat
 Another good solution:
