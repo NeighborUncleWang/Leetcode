0f7dde2e1c1c0b68b098ef63185e88ce89b5c522
Xianshu Li <lixianshu@utexas.edu>
2017-04-09 14:52:36 -0500

    Q419

--- Cpp/419_BattleshipsInABoard/001.cpp
+++ Cpp/419_BattleshipsInABoard/001.cpp
@@ -6,8 +6,8 @@ public:
         int count = 0;
         for (int i = 0; i < row; ++i) {
             for (int j = 0; j < column; ++j) {
-                if (board[i][j] == 'X' && (i == 0 || i > 0 && board[i - 1][j] != 'X') && 
-                (j == 0 || j > 0 && board[i][j - 1] != 'X')) {
+                if (board[i][j] == 'X' && (i == 0 || board[i - 1][j] != 'X') && 
+                (j == 0 || board[i][j - 1] != 'X')) {
                     ++count;
                 }
             }
