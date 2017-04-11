b62edc106aa6fca5e84d38169bd0bf50db2a3c95
Xianshu Li <lixianshu@utexas.edu>
2017-04-10 23:18:09 -0500

    Q424

--- Cpp/424_LongestRepeatingCharactersReplacement/001.cpp
+++ Cpp/424_LongestRepeatingCharactersReplacement/001.cpp
@@ -5,10 +5,12 @@ public:
         vector<int> count(26, 0);
         for (int left = 0, right = 0; right < s.size(); ++right) {
             max_count = max(max_count, ++count[s[right] - 'A']);
-            while (right - left + 1 - max_count > k) {
+            //这里可以写if而不是while
+            //因为每次要么是把整个window往右平移，要么是把整个window extend 1
+            if (right - left + 1 - max_count > k) {
                 --count[s[left++] - 'A'];
             }
-            result = max(result, right - left + 1 - max_count);
+            result = max(result, right - left + 1);
         }
         return result;
     }
