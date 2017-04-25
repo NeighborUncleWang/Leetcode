6f8a4db716a4840ea512288b4aa078410737eb35
Xianshu Li <lixianshu@utexas.edu>
2017-04-25 18:10:28 -0500

    Q053

--- Cpp/053_MaximumSubarray/001.cpp
+++ Cpp/053_MaximumSubarray/001.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+    	//必须把maxEndingHere初始化为nums[0]
+    	//然后让index从1开始，不能把maxEndingHere和maxSoFar都初始化成INT_MIN
+    	//然后让index从0开始，因为那样如果nums[0]为负值会导致maxEndingHere + nums[0]溢出
         int maxEndingHere = nums[0];
         int maxSoFar = nums[0];
         for (int i = 1; i < nums.size(); ++i) {
