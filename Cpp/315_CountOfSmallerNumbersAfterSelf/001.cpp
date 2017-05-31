a66a80b58a08b4d3b858d8f86e5a1f51105bb76c
Xianshu Li <lixianshu@utexas.edu>
2017-05-30 21:22:30 -0700

    Q315

--- Cpp/315_CountOfSmallerNumbersAfterSelf/001.cpp
+++ Cpp/315_CountOfSmallerNumbersAfterSelf/001.cpp
@@ -9,47 +9,41 @@ public:
         //     indices[i] = i;
         // }
         vector<int> result(numsSize, 0);
-        splitMerge(nums, 0, numsSize, indices, result);
+        Helper(nums, 0, numsSize, indices, result);
         return result;
     }
 private:
     //end is exclusive [start, end)
-    void splitMerge(vector<int>& nums, int start, int end, vector<int>& indices, vector<int>& result) {
+    void Helper(vector<int>& nums, int start, int end, vector<int>& indices, vector<int>& result) {
         //这里必须是<2，<1不行，会run time error，
         //因为start = 0, mid = 0, end = 1会导致无穷递归
         if (end - start < 2) {
             return;
         }
-        int middle = start + (end - start) / 2;
-        splitMerge(nums, start, middle, indices, result);
-        splitMerge(nums, middle, end, indices, result);
-        merge(nums, start, middle, end, indices, result);
-    }
-    void merge(vector<int>& nums, int start, int middle, int end, vector<int>& indices, vector<int>& result) {
-        int length = end - start;
-        vector<int> tempIndices;
-        int index1 = start;
-        int index2 = middle;
-        int rightCount = 0;
+        int mid = start + (end - start) / 2;
+        Helper(nums, start, mid, indices, result);
+        Helper(nums, mid, end, indices, result);
+        vector<int> temp(end - start);
+        int inversion = 0;
         //when we sort the elements, we should sort their indices in the original nums vector
         //so that we can update the corresponding value in the result vector
-        //唯一要用到的就是nums[indices[index1]] <= nums[indices[index2]]这里
+        //唯一要用到的就是nums[indices[i1]] <= nums[indices[i2]]这里
         //其他地方都用不到nums，都是对原来的indices进行操作
-        while (index1 < middle || index2 < end) {
-            //这里必须是nums[indices[index1]] <= nums[indices[index2]]
-            //不能是nums[indices[index1]] < nums[indices[index2]]
-            if (index1 < middle && (index2 >= end || nums[indices[index1]] <= nums[indices[index2]])) {
-                tempIndices.push_back(indices[index1]);
-                //这里必须是 += rightCount, 而不是 = rightCount;
-                //因为两个subarray merge之前result已经存有两个subarray里的reverse数值了
-                result[indices[index1]] += rightCount;
-                ++index1;
+        for (int i = 0, i1 = start, i2 = mid; i < end - start; ++i) {
+            //这里必须是nums[indices[i1]] <= nums[indices[i2]]
+            //不能是nums[indices[i1]] < nums[indices[i2]]
+            if (i1 < mid && (i2 == end || nums[indices[i1]] <= nums[indices[i2]])) {
+                temp[i] = indices[i1];
+                //这里必须是 += inversion, 而不是 = inversion;
+                //因为两个subarray merge之前result已经存有两个subarray里的inversion数值了
+                result[indices[i1]] += inversion;
+                ++i1;
             } else {
-                tempIndices.push_back(indices[index2]);
-                ++index2;
-                ++rightCount;
+                temp[i] = indices[i2];
+                ++i2;
+                ++inversion;
             }
         }
-        move(tempIndices.begin(), tempIndices.end(), indices.begin() + start);
+        move(temp.begin(), temp.end(), indices.begin() + start);
     }
 };
