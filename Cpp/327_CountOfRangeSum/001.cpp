c53520473a319ff775e792016dea9db3af5bf637
Xianshu Li <lixianshu@utexas.edu>
2017-05-30 21:58:34 -0700

    Q327

--- Cpp/327_CountOfRangeSum/001.cpp
+++ Cpp/327_CountOfRangeSum/001.cpp
@@ -6,35 +6,31 @@ public:
         for (int i = 0; i < n; ++i) {
             sums[i + 1] = sums[i] + nums[i];
         }
-        return SplitMerge(sums, 0, n + 1, lower, upper);
+        return Helper(sums, 0, n + 1, lower, upper);
     }
 private:
-    int SplitMerge(vector<long long>& sums, int start, int end, int lower, int upper) {
+    int Helper(vector<long long>& sums, int start, int end, int lower, int upper) {
         if (end - start < 2) {
             return 0;
         }
-        int middle = start + (end - start) / 2;
-        int count = SplitMerge(sums, start, middle, lower, upper)
-        + SplitMerge(sums, middle, end, lower, upper);
-        Merge(sums, start, middle, end, lower, upper, count);
-        return count;
-    }
-    void Merge(vector<long long>& sums, int start, int middle, int end, int lower, int upper, int& count) {
-        vector<long long> cache(end - start);
-        for (int i = start, j = middle, k = middle; i < middle; ++i) {
+        int mid = start + (end - start) / 2;
+        int res = Helper(sums, start, mid, lower, upper) + Helper(sums, mid, end, lower, upper);
+        for (int i = start, j = mid, k = mid; i < mid; ++i) {
             while (j < end && sums[j] - sums[i] < lower) ++j;
             while (k < end && sums[k] - sums[i] <= upper) ++k;
-            count += k - j;
+            res += k - j;
         }
-        for (int i = 0, iSmall = start, iLarge = middle; i < end - start; ++i) {
-            if (iSmall < middle && (iLarge >= end || sums[iSmall] <= sums[iLarge])) {
-                cache[i] = sums[iSmall];
-                ++iSmall;
-            } else {
-                cache[i] = sums[iLarge];
-                ++iLarge;
-            }
-        }
-        move(cache.begin(), cache.end(), sums.begin() + start);
+        // vector<long long> cache(end - start);
+        // for (int i = 0, iSmall = start, iLarge = middle; i < end - start; ++i) {
+        //     if (iSmall < middle && (iLarge >= end || sums[iSmall] <= sums[iLarge])) {
+        //         cache[i] = sums[iSmall];
+        //         ++iSmall;
+        //     } else {
+        //         cache[i] = sums[iLarge];
+        //         ++iLarge;
+        //     }
+        // }
+        inplace_merge(sums.begin() + start, sums.begin() + mid, sums.begin() + end);
+        return res;
     }
 };
