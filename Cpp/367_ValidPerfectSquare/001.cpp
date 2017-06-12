5d005eb8d600f21eb1247c3e80ddc1342cf538b6
Xianshu Li <lixianshu@utexas.edu>
2017-06-12 00:21:26 -0500

    Q367

--- Cpp/367_ValidPerfectSquare/001.cpp
+++ Cpp/367_ValidPerfectSquare/001.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        //this is to avoid the case middle == 0
-        if (num == 1) return true;
-        int low = 0;
-        int high = num;
+        int low = 1, high = num;
         while (low <= high) {
-            int middle = low + (high - low) / 2;
-            //or we can use long long middle then test middle * middle <= num
-            //不要写if (middle == num / middle) return middle
-            //因为num = 5 会导致return 2
-            if (middle <= num / middle) {
-                low = middle + 1;
+            int mid = low + (high - low) / 2;
+            if (mid * mid == num) {
+                return true;
+            } else if (mid < num / mid) {
+                low = mid + 1;
             } else {
-                high = middle - 1;
+                high = mid - 1;
             }
         }
-        return high * high == num;
+        return false;
     }
 };
