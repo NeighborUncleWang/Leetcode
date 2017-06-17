31268de71ae659a7dcd090d035fd49f2239b608f
Xianshu Li <lixianshu@utexas.edu>
2017-06-17 15:31:10 -0500

    Q273

--- Cpp/273_IntegerToEnglishWords/001.cpp
+++ Cpp/273_IntegerToEnglishWords/001.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     string numberToWords(int num) {
+        vector<string> less_than_twenty{"", " One", " Two", " Three", " Four", " Five", 
+        " Six", " Seven", " Eight", " Nine", " Ten", " Eleven", " Twelve", " Thirteen", 
+        " Fourteen", " Fifteen", " Sixteen", " Seventeen", " Eighteen", " Nineteen"};
+        vector<string> tens{"", "", " Twenty", " Thirty", " Forty", " Fifty", 
+        " Sixty", " Seventy", " Eighty", " Ninety"};
         vector<string> thousands{"", " Thousand", " Million", " Billion"};
-        string result;
+        string res;
         for (int i = 0; num > 0; num /= 1000, ++i) {
             //if(num % 1000)这个判断必须有，否则1,000,000会输出One Million Thousand
             if (num % 1000) {
-                result = helper(num % 1000) + thousands[i] + result;
+                res = helper(num % 1000, less_than_twenty, tens) + thousands[i] + res;
             }
         }
-        return result.empty() ? "Zero" : result.substr(1);
+        return res == "" ? "Zero" : res.substr(1);
     }
 private:
-    string helper(int num) {
-        vector<string> lessThan20{
-            "", " One", " Two", " Three", " Four", " Five", " Six", " Seven", " Eight", " Nine",
-            " Ten", " Eleven", " Twelve", " Thirteen", " Fourteen",  " Fifteen", " Sixteen", " Seventeen",
-            " Eighteen", " Nineteen"
-        };
-        vector<string> tens{
-            "", "", " Twenty", " Thirty", " Forty", " Fifty", " Sixty", " Seventy", " Eighty", " Ninety"
-        };
+    string helper(int num, vector<string>& less_than_twenty, vector<string>& tens) {
         if (num < 20) {
-            return lessThan20[num];
+            return less_than_twenty[num];
         } else if (num < 100) {
-            return tens[num / 10] + lessThan20[num % 10];
+            return tens[num / 10] + less_than_twenty[num % 10];
         } else {
-            return lessThan20[num / 100] + " Hundred" + helper(num % 100);
+            return less_than_twenty[num / 100] + " Hundred" + helper(num % 100, less_than_twenty, tens);
         }
     }
 };
--- Cpp/301_RemoveInvalidParentheses/readme.txt
+++ Cpp/301_RemoveInvalidParentheses/readme.txt
@@ -1,8 +1,9 @@
 001.cpp:
-Time complexity: O(n*2^n)
-Space complexity: O(n*2^n)
+Time complexity: O(2^n)
+Space complexity: O(2^n)
 What's the time complexity for this solution?
 Is it O(2^n) or O(n*2^n)? Actually this solution is much faster than 002.cpp.
+我觉得001.cpp是O(2^n),感觉002.cpp是O(n*2^n),因为001.cpp只是产生2^n种情况，并没有对2^n种情况进行过O(n)的操作
 Reference:
 https://leetcode.com/discuss/72208/easiest-9ms-java-solution
 002.cpp:
