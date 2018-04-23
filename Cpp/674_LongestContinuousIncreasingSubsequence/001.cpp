a6ecee54f74dc861889e22ffe7f4764acc520284
HelloWorld <lixianshu1992@gmail.com>
2018-04-22 23:09:23 -0700

    Q674

---
+++ Cpp/674_LongestContinuousIncreasingSubsequence/001.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        int res = 0, cur = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i == 0 || nums[i] > nums[i - 1]) {
+                ++cur;
+                res = max(res, cur);
+            } else {
+                cur = 1;
+            }
+        }
+        return res;
+    }
+};
