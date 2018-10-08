0c7bf98956b6570341d1752735a7edaf72120f87
HelloWorld <lixianshu1992@gmail.com>
2018-10-07 21:51:06 -0700

    Q684

---
+++ Cpp/684_RedundantConnection/001.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<int> weights(n + 1, 1);
+        vector<int> roots(n + 1, 0);
+        iota(roots.begin() + 1, roots.end(), 1);
+        for (auto& edge : edges) {
+            int root1 = findRoot(edge[0], roots);
+            int root2 = findRoot(edge[1], roots);
+            if (root1 == root2) {
+                return edge;
+            } else {
+                if (weights[root1] < weights[root2]) {
+                    roots[root1] = root2;
+                    weights[root2] += weights[root1];
+                } else {
+                    roots[root2] = root1;
+                    weights[root1] += weights[root2];
+                }
+            }
+        }
+    }
+private:
+    int findRoot(int index, vector<int>& roots) {
+        while (index != roots[index]) {
+            roots[index] = roots[roots[index]];
+            index = roots[index];
+        }
+        return index;
+    }
+};
---
+++ Cpp/684_RedundantConnection/readme.txt
@@ -0,0 +1,7 @@
+001.cpp:
+Time complexity: O(nlog*n)
+Space complexity: O(n)
+Recursive version.
+Reference:
+https://leetcode.com/problems/redundant-connection/solution/
+https://leetcode.com/problems/redundant-connection/discuss/107984/10-line-Java-solution-Union-Find.
