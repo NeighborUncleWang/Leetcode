03be03a8bb9c7e16cdaff908735bc449710054af
HelloWorld <lixianshu1992@gmail.com>
2018-10-28 21:57:08 -0700

    Q818

---
+++ Cpp/818_RaceCar/001.cpp
@@ -0,0 +1,15 @@
+class Solution {
+private:
+    vector<int> dp = vector<int>(1001, 0);
+public:
+    int racecar(int target) {
+        if (dp[target] > 0) return dp[target];
+        int n = log2(target + 1);
+        if (target == (1 << n) - 1) return dp[target] = n;
+        dp[target] = racecar(pow(2, n + 1) - 1 - target) + n + 2;
+        for (int i = 1; i < n; ++i) {
+            dp[target] = min(dp[target], racecar(target - pow(2, n) + pow(2, i)) + n + i + 2);
+        }
+        return dp[target];
+    }
+};
---
+++ Cpp/818_RaceCar/readme.txt
@@ -0,0 +1,6 @@
+001.cpp:
+Time complexity: O(n^2) (n entries and each entry takes O(n) to get)
+Space complexity: O(1)
+Reference:
+https://blog.csdn.net/magicbean2/article/details/80333734
+https://leetcode.com/problems/race-car/discuss/123834/C%2B%2BJavaPython-DP-solution
