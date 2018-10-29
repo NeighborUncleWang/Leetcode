f3dc38792203f93e58f3b55d49580568e903dbc2
HelloWorld <lixianshu1992@gmail.com>
2018-10-28 19:50:30 -0700

    Q835

--- Cpp/835_ImageOverlap/001.cpp
+++ Cpp/835_ImageOverlap/001.cpp
@@ -1,24 +1,37 @@
+struct myhash {
+    int operator() (pair<int, int> key) const {
+        return key.first * 10 + key.second;
+    }
+};
 class Solution {
 public:
     int largestOverlap(vector<vector<int>>& A, vector<vector<int>>& B) {
         int n = A.size();
-        vector<int> indicesA, indicesB;
-        for (int i = 0; i < n * n; ++i) {
-            if (A[i / n][i % n]) {
-                indicesA.push_back(i / n * 100 + i % n);
-            }
-            if (B[i / n][i % n]) {
-                indicesB.push_back(i / n * 100 + i % n);
+        vector<pair<int, int>> locA, locB;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (A[i][j]) {
+                    locA.emplace_back(i, j);
+                }
+                if (B[i][j]) {
+                    locB.emplace_back(i, j);
+                }
             }
         }
-        unordered_map<int, int> count;
-        for (int idxA : indicesA) {
-            for (int idxB : indicesB) {
-                ++count[idxA - idxB];
+        auto hash = [=] (auto key) {
+            return key.first * n + key.second;
+        };
+        unordered_map<pair<int, int>, int, decltype(hash)> count(10, hash);
+        //下面是用strcut myhash 初始化 unordered_map的方法
+        //其中myhash operator()后面必须加const，不然会出错
+        // unordered_map<pair<int, int>, int, myhash> count;
+        for (auto& loc1 : locA) {
+            for (auto& loc2 : locB) {
+                ++count[{loc1.first - loc2.first, loc1.second - loc2.second}];
             }
         }
         int res = 0;
-        for (auto it : count) {
+        for (auto& it : count) {
             res = max(res, it.second);
         }
         return res;
